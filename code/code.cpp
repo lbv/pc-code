a1bc46fd2cece453ae65517a2a0b55f97bb0ca13
Leonardo Boshell <leonardo@diptongonante.com>
2012-07-03 04:11:03 -0500

    Added code for quickselect

--- code/code.cpp
+++ code/code.cpp
@@ -837,6 +837,37 @@ void days_to_date(int days, int &d, int &m, int &y)
 }
 
 //
+// Quickselect - for locating the median
+//
+struct Data {
+    int v;  // value
+    int n;  // occurrences
+    int p;  // original position of data, to break ties
+    Data(int V, int N, int P) : v(V), n(N), p(P) {}
+    bool operator<(const Data &x) const {
+        if (v != x.v) return v < x.v;
+        if (n != x.n) return n < x.n;
+        return p < x.p;
+    }
+};
+typedef vector<Data> DV;
+typedef DV::iterator DVi;
+int select_kth(DVi lo, DVi hi, int k)
+{
+    if (hi == lo + 1) return lo->v;
+    swap(*(lo + (rand() % (hi - lo))), *(hi - 1));
+    Data piv = *(hi - 1);
+    DVi x = lo;
+    int lt = 0;
+    for (DVi i = lo, I = hi - 1; i != I; ++i)
+        if (*i < piv) { swap(*i, *x); lt += x->n, ++x; }
+    swap(*x, *(hi - 1));
+    if (k < lt) return select_kth(lo, x, k);
+    if (k >= lt + x->n) return select_kth(x + 1, hi, k - lt - x->n);
+    return x->v;
+}
+
+//
 // Misc functions
 //
 // next higher number with same number of 1's in binary

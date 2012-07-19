4528176b7b5ce6ea645820dbd53e7dc61829e00c
Leonardo Boshell <leonardo@diptongonante.com>
2012-07-19 14:04:21 -0500

    Added case

---
+++ solved/brush-iii/gen.cpp
@@ -0,0 +1,53 @@
+/*
+	Author       :	Jan
+	Problem Name :
+	Algorithm    :
+	Complexity   :
+*/
+
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int cases;
+
+int main() {
+	freopen("dd.in", "w", stdout);
+
+	cases = 99;
+	printf("%d\n", cases);
+	while( cases-- ) {
+		int n = 100 - rand() % 10;
+		int w = rand() % 10000 + 1;
+		int k = rand() % 100 + 1;
+
+		k = rand() % 10 + 5;
+
+		w = rand() % 10 + 10;
+		int A[105];
+		printf("\n%d %d %d\n", n, w, k);
+		int prev = 0;
+		for( int i = 0; i < n; i++ ) {
+			A[i] = prev + rand() % w + 1;
+			prev = A[i];
+		}
+		for( int i = 0; i < n; i++ ) {
+			printf("0 %d\n", A[i]);
+		}
+	}
+	return 0;
+}
+

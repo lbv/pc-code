d2ca8ccdf2b26989cb02bb532cc562d64b7af4c0
Leonardo Boshell <leonardo@diptongonante.com>
2012-07-23 14:56:02 -0500

    Added cases

--- solved/brush-iii/gen.cpp
+++ solved/brush-iii/gen.cpp
@@ -25,16 +25,19 @@ using namespace std;
 int cases;
 
 int main() {
-	freopen("dd.in", "w", stdout);
+	//freopen("dd.in", "w", stdout);
 
 	cases = 99;
+	srand(time(NULL));
 	printf("%d\n", cases);
 	while( cases-- ) {
-		int n = 100 - rand() % 10;
+		//int n = 100 - rand() % 10;
+		int n = rand() % 10 + 1;
 		int w = rand() % 10000 + 1;
 		int k = rand() % 100 + 1;
 
-		k = rand() % 10 + 5;
+		//k = rand() % 10 + 5;
+		k = rand() % 8 + 2;
 
 		w = rand() % 10 + 10;
 		int A[105];

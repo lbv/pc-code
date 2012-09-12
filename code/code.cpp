bb5440d114b900f12567900ee4d44b00b3946f4c
Leonardo Boshell <leonardo@diptongonante.com>
2012-09-12 13:49:58 -0500

    Added macro for counting bitwise trailing zeros

--- code/code.cpp
+++ code/code.cpp
@@ -995,6 +995,14 @@ u32 next_popcount(u32 n)
 u32 init_popcount(int n) { return (1 << n) - 1; }
 #define Back(b)    ((b) & -(b))
 #define PopBack(b) (b &= ~Back(b))
+// values of (1 << x) for x<32 are unique modulo 37
+static const int m37pos[] = {
+    32,  0,  1, 26,  2, 23, 27,  0,  3,
+    16, 24, 30, 28, 11,  0, 13,  4,  7,
+    17,  0, 25, 22, 31, 15, 29, 10, 12,
+     6,  0, 21, 14,  9,  5, 20,  8, 19, 18
+};
+#define Ctz(x) (m37pos[(x) % 37])
 // Finds the most significant bit set on n. The bit is left in b, and its
 // zero-indexed position in p
 void msb(i64 n, i64 &b, int &p)

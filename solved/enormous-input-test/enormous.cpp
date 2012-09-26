37d0da46262d71b00e5bf53f3323c02593b994c7
Leonardo Boshell <leonardo@diptongonante.com>
2012-09-26 15:53:12 -0500

    Simplified reader code

--- solved/enormous-input-test/enormous.cpp
+++ solved/enormous-input-test/enormous.cpp
@@ -1,16 +1,13 @@
 #include <cstdio>
 
 typedef unsigned int u32;
-#define BUF 65536
-class Reader {
-    char buf[BUF]; char b; int bi, bz;
-public:
-    Reader() { bi=bz=0; read(); }
-    void read() {
-        if (bi==bz) { bi=0; bz = fread(buf, 1, BUF, stdin); }
-        b = bz ? buf[bi++] : 0; }
+
+struct Reader {
+    char b;
+    Reader() { read(); }
+    void read() { int r = fgetc_unlocked(stdin); b = r == EOF ? 0 : r; }
     void skip() { while (b > 0 && b <= 32) read(); }
-    u32 nextU32() {
+    u32 next_u32() {
         u32 v = 0; for (skip(); b > 32; read()) v = v*10 + b-48; return v; }
 };
 
@@ -18,12 +15,13 @@ u32 n, k, i, d;
 
 int main(void)
 {
-    Reader _r;
-    n = _r.nextU32();
-    k = _r.nextU32();
+    Reader rr;
+
+    n = rr.next_u32();
+    k = rr.next_u32();
     d = 0;
     while (n--) {
-        i = _r.nextU32();
+        i = rr.next_u32();
         if (i % k == 0) d++;
     }
     printf ("%d\n", d);

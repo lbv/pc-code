2218fc12a4defb970b41cd0f7f8d9692a53298b6
Leonardo Boshell <leonardo@diptongonante.com>
2012-10-15 23:49:21 -0500

    Added generator, cases

---
+++ solved/a-wedding-party/gen.cpp
@@ -0,0 +1,78 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+using namespace std;
+
+
+#define MAXT 50
+
+/*
+#define MAXS 15
+#define MAXN 500
+#define MAXM 10000
+#define MAXW 100
+*/
+
+#define MAXS 3
+#define MAXN 5
+#define MAXM 8
+#define MAXW 100
+
+
+#define Zero(v) memset((v), 0, sizeof(v))
+
+
+bool edges[MAXN][MAXN];
+bool shops[MAXN];
+
+
+void test_case()
+{
+    int N = rand() % (MAXN - 1) + 2;
+
+    int TopM = min(MAXM, N * (N-1));
+    int M = rand() % TopM + 1;
+
+    int TopS = min(N, MAXS);
+    int S = rand() % (TopS + 1);
+
+    printf("%d %d %d\n", N, M, S);
+
+    Zero(shops);
+    for (int i = 0; i < S; ++i) {
+        int s;
+        do {
+            s = rand() % N;
+        } while (shops[s]);
+        shops[s] = true;
+        printf("%s%d", i == 0 ? "" : " ", s);
+    }
+    putchar('\n');
+
+    Zero(edges);
+    while (M--) {
+        int u, v, w;
+        do {
+            u = rand() % N;
+            v = rand() % N;
+        } while (u == v || edges[u][v]);
+        edges[u][v] = true;
+
+        w = rand() % MAXW + 1;
+        printf("%d %d %d\n", u, v, w);
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    int T = MAXT;
+    printf("%d\n", T);
+
+    while (T--) test_case();
+
+    return 0;
+}

7c976124cc98de920b3ab4259cac0068ce5a9e9e
Leonardo Boshell <leonardo@diptongonante.com>
2012-11-18 23:05:34 -0500

    Editions to Segment Tree code, added BST code

--- code/code.cpp
+++ code/code.cpp
@@ -960,55 +960,56 @@ struct Bit {
 #define MAXN 1024000
 #define MAXH 21  // 1 + ceil(log2(MAXN))
 struct SegTree {
-    IV A, T;
+    struct Node {
+        int v;   // tree value
+        bool u;  // requires update
+    };
+
+    Node T[1 << MAXH];
     int n;
-    SegTree() {
-        A.resize(MAXN);
-        T.resize(1 << MAXH);
-    }
 
     void init(int N) { n = N; tree_init(1, 0, n-1); }
     void tree_init(int x, int a, int b) {
-        if (a == b) { T[x] = a; return; }
+        T[x].u = false;
+        if (a == b) { T[x].v = 1; return; }
         int lt = 2*x, rt = lt + 1, md = (a+b)/2;
         tree_init(lt, a, md);
         tree_init(rt, md + 1, b);
-        T[x] = A[T[lt]] <= A[T[rt]] ? T[lt] : T[rt];
+        T[x].v = T[lt].v + T[rt].v;
     }
 
     void propagate(int x, int a, int b) {
-        if (U[x] == 0) return;
-        T[x] += U[x] * (b - a + 1);
+        if (! T[x].u) return;
+        T[x].v = 0;
         if (a != b) {
             int lt = 2*x, rt = lt + 1;
-            U[lt] += U[x];
-            U[rt] += U[x];
+            T[lt].u = T[rt].u = true;
         }
-        U[x] = 0;
+        T[x].u = false;
     }
 
-    void update(int i, int j, i64 v) { tree_update(i, j, v, 1, 0, n-1); }
-    void tree_update(int i, int j, i64 v, int x, int a, int b) {
+    void update(int i, int j, int v) { tree_update(i, j, v, 1, 0, n-1); }
+    void tree_update(int i, int j, int v, int x, int a, int b) {
         propagate(x, a, b);
         if (j < a || i > b) return;
-        if (a == b) { T[x] += v; return; }
+        if (a == b) { T[x].v += v; return; }
         int lt = 2*x, rt = lt + 1, md = (a+b)/2;
         if (a >= i && b <= j) {
-            T[x] += v * (b - a + 1);
-            U[lt] += v;
-            U[rt] += v;
+            T[x].v += v * (b - a + 1);
+            T[lt].u += v;
+            T[rt].u += v;
             return;
         }
         tree_update(i, j, v, lt, a, md);
         tree_update(i, j, v, rt, md + 1, b);
-        T[x] = T[rt] + T[lt];
+        T[x].v = T[rt].v + T[lt].v;
     }
 
     int query(int i, int j) { return tree_query(i, j, 1, 0, n-1); }
     int tree_query(int i, int j, int x, int a, int b) {
         if (j < a || i > b) return -1;
         propagate(x, a, b);
-        if (a >= i && b <= j) return T[x];
+        if (a >= i && b <= j) return T[x].v;
         int lt = 2*x, rt = lt + 1, md = (a+b)/2;
         int q1 = tree_query(i, j, lt, a, md);
         int q2 = tree_query(i, j, rt, md + 1, b);
@@ -1040,6 +1041,68 @@ struct SpTable {
         int k = log2(j - i + 1);
         int a = M[i][k], b = M[j + 1 - (1<<k)][k];
         return A[a] <= A[b] ? a : b;
+    }
+};
+
+//
+// Binary Search Tree
+//
+const int Nil = 0;
+#define LT(t)  (T[T[t].l])
+#define RT(t)  (T[T[t].r])
+#define PAR(t) (T[T[t].p])
+struct Bst {
+    struct Node {
+        int p, l, r, s, k;  // parent, left, right, size, key
+        void init(int K) { k = K; p = l = r = Nil; s = 1; }
+        void clear() { p = l = r = s = k = 0; }
+    };
+    int K[MAXK];
+    Node T[MAXK + 1];
+    int root, sz;
+    int get(int t) { return K[T[t].k]; }
+    int size() { return T[root].s; }
+    void init(int n) { T[0].clear(); sz = 0; root = init_tree(0, n - 1); }
+    int init_tree(int a, int b) {
+        if (a > b) return Nil;
+        int t = ++sz;
+        if (a == b) { T[t].init(a); return t; }
+        int m = (a + b) / 2;
+        T[t].l = init_tree(a, m - 1);
+        T[t].r = init_tree(m + 1, b);
+        T[t].p = Nil, T[t].k = m, T[t].s = 1 + LT(t).s + RT(t).s;
+        LT(t).p = t; RT(t).p = t;
+        return t;
+    }
+    int find_kth(int k) { return find_kth_tree(k, root); }
+    int find_kth_tree(int k, int t) {
+        if (LT(t).s > k) return find_kth_tree(k, T[t].l);
+        k -= LT(t).s;
+        if (k == 0) return t;
+        if (--k >= RT(t).s) return Nil;
+        return find_kth_tree(k, T[t].r);
+    }
+    void remove(int t) {
+        int x;
+        if (T[t].l != Nil && T[t].r != Nil) {
+            if (RT(t).s >= LT(t).s) for (x=T[t].r; T[x].l != Nil; x=T[x].l) ;
+            else                    for (x=T[t].l; T[x].r != Nil; x=T[x].r) ;
+            T[t].k = T[x].k;
+            t = x;
+        }
+        if (T[t].l == Nil && T[t].r == Nil) {
+            if (T[t].p == Nil) { root = Nil; return; }
+            if (PAR(t).l == t) PAR(t).l = Nil;
+            else               PAR(t).r = Nil;
+        }
+        else {
+            x = T[t].l != Nil ? T[t].l : T[t].r;
+            if (T[t].p == Nil) { T[root = x].p = Nil; return; }
+            if (PAR(t).l == t) PAR(t).l = x;
+            else               PAR(t).r = x;
+            T[x].p = T[t].p;
+        }
+        for (x = T[t].p; x != Nil; x = T[x].p) --T[x].s;
     }
 };
 

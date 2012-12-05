032ed71b9e58800a17376e6e7ed77ed400e4e335
Leonardo Boshell <leonardo@diptongonante.com>
2012-12-04 22:49:46 -0500

    Added problem: Looking for a Subset

---
+++ solved/looking-for-a-subset/lightoj/looking.cpp
@@ -0,0 +1,94 @@
+#include <cstdio>
+
+
+#define MAXN 100000
+
+const int NEGINF = -100000000 - 10;
+
+
+typedef unsigned int u32;
+
+
+struct Reader {
+    int b; Reader() { read(); }
+    void read() { b = getchar_unlocked(); }
+    void skip() { while (b >= 0 && b <= 32) read(); }
+
+    u32 next_u32() {
+        u32 v = 0; for (skip(); b > 32; read()) v = 10*v+b-48; return v; }
+    int next_int() {
+        int v = 0; bool s = false;
+        skip(); if (b == '-') { s = true; read(); }
+        for (; b > 32; read()) v = v*10 + b-48; return s ? -v : v; }
+};
+
+
+int n, q, m;
+
+int rev[MAXN];  // array of integers in reverse order
+
+int len_lds[MAXN];
+int I[MAXN + 1];
+
+
+int lds(int *seq, int *ls)
+{
+    int len = 0;
+    for (int i = 0; i < n; ++i) {
+        int lo = 1, hi = len;
+        while (lo <= hi) {
+            int m = (lo + hi) / 2;
+            if (I[m] > seq[i]) lo = m + 1;
+            else hi = m - 1;
+        }
+        I[lo] = seq[i], ls[i] = lo;
+        if (len < lo) len = lo;
+    }
+    return len;
+}
+
+void solve()
+{
+    int last = NEGINF;
+    int len = m;
+
+    int cnt = 0;
+    for (int i = n - 1; len > 0 && i >= 0; --i)
+        if (len_lds[i] >= len && rev[i] > last) {
+            if (cnt > 0) putchar(' ');
+            printf("%d", rev[i]);
+            ++cnt;
+            --len;
+            last = rev[i];
+        }
+
+    if (cnt == 0)
+        puts("Impossible");
+    else
+        putchar('\n');
+}
+
+int main()
+{
+    Reader rr;
+    int T = rr.next_u32();
+
+    int ncase = 0;
+    while (T--) {
+        n = rr.next_u32();
+        q = rr.next_u32();
+
+        for (int i = n - 1; i >= 0; --i)
+            rev[i] = rr.next_int();
+
+        printf("Case %d:\n", ++ncase);
+        lds(rev, len_lds);
+
+        while (q--) {
+            m = rr.next_u32();
+            solve();
+        }
+    }
+
+    return 0;
+}

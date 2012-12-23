a6cf6cd2833fd78a4a3583588b0c0a72d20aa0f6
Leonardo Boshell <leonardo@diptongonante.com>
2012-12-22 20:23:22 -0500

    Reimplemented generator, added cases

--- solved/monkey-banana-problem/gen.cpp
+++ solved/monkey-banana-problem/gen.cpp
@@ -1,83 +1,54 @@
-/*
-	Author       :	Jan
-	Problem Name :
-	Algorithm    :
-	Complexity   :
-*/
-
-#include <set>
-#include <map>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <cctype>
 #include <cstdio>
-#include <string>
-#include <vector>
-#include <cassert>
 #include <cstdlib>
-#include <cstring>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
+#include <ctime>
 
-using namespace std;
 
-int getRandom( int cases ) {
-	int k;
-	while(1) {
-		k = rand() % 32767 + 1;
-		if( cases == 20 ) {
-			if( k >= 30000 ) break;
-		}
-		else if( cases == 30 ) {
-			if( k <= 100 ) break;
-		}
-		else if( cases == 40 ) {
-			if( k <= 10 ) break;
-		}
-		else break;
-	}
-	return k;
+#if 0
+#define MAXT 50
+#define MAXN 100
+const int MAXI = (1 << 15) - 1;
+#endif
+
+#if 1
+#define MAXT 20
+#define MAXN 9
+const int MAXI = 100;
+#endif
+
+
+void test_case(bool crit = false)
+{
+    int N = crit ? MAXN : rand() % MAXN + 2;
+    printf("%d\n", N);
+
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (j > 0) putchar(' ');
+            int n = rand() % MAXI + 1;
+            printf("%d", n);
+        }
+        putchar('\n');
+    }
+
+    for (int i = 1; i < N; ++i) {
+        for (int j = 0, J = N - i; j < J; ++j) {
+            if (j > 0) putchar(' ');
+            int n = rand() % MAXI + 1;
+            printf("%d", n);
+        }
+        putchar('\n');
+    }
 }
 
-int main() {
-	//freopen("e.in", "w", stdout);
+int main()
+{
+    srand(time(NULL));
 
-	double cl = clock();
+    int T = MAXT;
+    printf("%d\n", T);
 
-	int cases = 50;
+    test_case(true); --T;
+    while (T--) test_case();
 
-	printf("%d\n", cases);
-	while( cases-- ) {
-		int n = 100 - rand() % 10;
-
-		if( cases % 5 == 0 ) n = 100;
-		if( cases <= 3 ) n = cases + 1;
-
-		printf("%d\n", n);
-		for( int i = 0; i < n; i++ ) {
-			for( int j = 0; j <= i; j++ ) {
-				if( j ) putchar(' ');
-				printf("%d", getRandom(cases));
-			}
-			puts("");
-		}
-		for( int i = 0; i < n - 1; i++ ) {
-			for( int j = 0; j < n - i - 1; j++ ) {
-				if( j ) putchar(' ');
-				printf("%d", getRandom(cases));
-			}
-			puts("");
-		}
-	}
-
-	cl = clock() - cl;
-	fprintf(stderr, "Total Execution Time = %lf seconds\n", cl / CLOCKS_PER_SEC);
-
-	return 0;
+    return 0;
 }
-

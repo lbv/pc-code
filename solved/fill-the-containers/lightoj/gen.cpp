eea1e93a62d2d67db2dbec7fa7d4962d5e3cde34
Leonardo Boshell <leonardo@diptongonante.com>
2012-12-26 19:06:37 -0500

    Added extra cases

--- solved/fill-the-containers/lightoj/gen.cpp
+++ solved/fill-the-containers/lightoj/gen.cpp
@@ -3,27 +3,27 @@
 #include <ctime>
 
 
-#if 1
+#if 0
 #define MAXT 100
 #define MAXN 1000
 #define MAXM 1000000
 #define MAXC 1000000
 #endif
 
-#if 0
-#define MAXT 100
-#define MAXN 10
-#define MAXM 1000000
-#define MAXC 100
+#if 1
+#define MAXT 20
+#define MAXN 20
+#define MAXM 30
+#define MAXC 200
 #endif
 
 
 void test_case()
 {
     int n = rand() % MAXN + 1;
-    int m = rand() % n + 1;
+    int m = rand() % (n + 1) + 1;
 
-    if (rand() % 20 == 0)
+    if (rand() % 8 == 0)
         m = rand() % MAXM + 1;
 
     printf("%d %d\n", n, m);

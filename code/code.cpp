93283a0f8c65f8cc40c1e5bb28fdd05e5c6c6546
Leonardo Boshell <leonardo@diptongonante.com>
2013-01-03 07:10:58 -0500

    Changes to common code
    * Added U128 struct
    * Use templates for certain functions and structs
    * Minor corrections

--- code/code.cpp
+++ code/code.cpp
@@ -497,7 +497,7 @@ namespace Num
             int q = 1;
             while (n % p == 0) {
                 q *= p; n /= p;
-                cFor(IV, v, ds) aux.push_back(*v * q);
+                For(IV, v, ds) aux.push_back(*v * q);
             }
             sn = sqrt(n); ds = aux;
         }
@@ -520,11 +520,16 @@ namespace Num
         for (i64 b=_b; e; e >>= 1, b = b*b%m) if (e & 1) res = res*b%m;
         return res;
     }
-    int ipow(int b, int e) {
-        int res = 1; for (; e; e >>= 1, b *= b) if (e & 1) res *= b;
+
+    template<typename TB, typename TE>
+    TB sq_pow(TB b, TE e) {
+        TB res = 1; for (; e; e >>= 1, b *= b) if (e & 1) res *= b;
         return res;
     }
-    int gcd(int a, int b) { for (int c = a%b; c; a=b,b=c,c=a%b); return b; }
+
+    template<typename T>
+    T gcd(T a, T b) { for (T c = a%b; c != 0; a=b,b=c,c=a%b); return b; }
+
     void ext_euclid(int a, int b, int &x, int &y, int &gcd) {
         x = 0; y = 1; gcd = b;
         int m, n, q, r;
@@ -573,7 +578,7 @@ namespace Num
 #define BIFMT "%03d"
 struct Bigint {
     IV d; bool sgn;
-    Bigint(i64 n=0) {
+    Bigint(int n=0) {
         if (n < 0) sgn = true, n = -n; else sgn = false;
         if (n < BIBAS) d.push_back(n);
         else while (n != 0) { d.push_back(n % BIBAS); n /= BIBAS; }
@@ -682,18 +687,14 @@ struct Bigint {
         tmp *= *this;
         return tmp;
     }
-    void println() {
+    void print(bool nl = true) {
         if (sgn) putchar('-');
         bool flg = true;
         RFor (IV, i, d) {
             if (flg) { printf("%d", *i); flg=false; }
             else printf(BIFMT, *i);
-        } putchar('\n');
-    }
-    int to_int() {
-        int res = 0, p = 1;
-        for (int i=0, I=len(); i < I; i++) { res += d[i] * p; p *= BIBAS; }
-        return sgn ? -res : res;
+        }
+        if (nl) putchar('\n');
     }
     string to_string() {
         char buf[BIDIG+1]; string str;
@@ -706,18 +707,172 @@ struct Bigint {
         }
         return str;
     }
+    int to_int() {
+        int res = 0, p = 1;
+        for (int i=0, I=len(); i < I; i++) { res += d[i] * p; p *= BIBAS; }
+        return sgn ? -res : res;
+    }
+    Bigint operator+(const Bigint x) const {
+        Bigint ans = *this; ans += x; return ans; }
+    Bigint operator-(const Bigint x) const {
+        Bigint ans = *this; ans -= x; return ans; }
+    Bigint operator*(const Bigint x) const {
+        Bigint ans = *this; ans *= x; return ans; }
+    Bigint operator/(const Bigint x) const {
+        Bigint ans = *this; ans /= x; return ans; }
+    Bigint operator%(const Bigint x) const {
+        Bigint ans = *this; ans %= x; return ans; }
+};
+
+//
+// 128-bit unsigned integer
+//
+struct U128 {
+    u64 lo, hi;
+    static const size_t sz  = 128;
+    static const size_t hsz = 64;
+    static const u64 bmax = -1;
+
+    U128() : lo(0), hi(0) {}
+    U128(int v) : lo(v), hi(0) {}
+
+    U128 operator-() const { return ~U128(*this) + 1; }
+    U128 operator~() const {
+		U128 t(*this);
+		t.lo = ~t.lo;
+		t.hi = ~t.hi;
+		return t;
+	}
+
+    U128 &operator+=(const U128 &b) {
+        if (lo > bmax - b.lo) ++hi;
+        lo += b.lo;
+        hi += b.hi;
+        return *this;
+    }
+    U128 &operator-=(const U128 &b) { return *this += -b; }
+    U128 &operator*=(const U128 &b) {
+        if (*this == 0 || b == 1) return *this;
+        if (b == 0) { lo = hi = 0; return *this; }
+
+        U128 a(*this);
+        U128 t = b;
+
+        lo = hi = 0;
+
+        for (size_t i = 0; i < sz; ++i) {
+            if((t & 1) != 0) *this += (a << i);
+            t >>= 1;
+        }
+        return *this;
+    }
+    U128 &operator/=(const U128 &b) {
+		U128 rem;
+		divide(*this, b, *this, rem);
+		return *this;
+    }
+    U128 &operator%=(const U128 &b) {
+		U128 quo;
+		divide(*this, b, quo, *this);
+		return *this;
+    }
+
+    static void
+    divide(const U128 &num, const U128 &den, U128 &quo, U128 &rem) {
+        if(den == 0) { int a = 0; quo = U128(a/a); }
+        U128 n = num, d = den, x = 1, ans = 0;
+
+        while((n >= d) && (((d >> (sz - 1)) & 1) == 0)) {
+            x <<= 1;
+            d <<= 1;
+        }
+
+        while(x != 0) {
+            if(n >= d) {
+                n -= d;
+                ans |= x;
+            }
+
+            x >>= 1;
+            d >>= 1;
+        }
+
+        quo = ans, rem = n;
+    }
+
+    U128 &operator&=(const U128 &b) { hi &= b.hi; lo &= b.lo; return *this; }
+    U128 &operator|=(const U128 &b) {
+        hi |= b.hi;
+        lo |= b.lo;
+        return *this;
+    }
+    U128 &operator<<=(const U128& rhs) {
+		size_t n = rhs.to_int();
+        if (n >= sz) { lo = hi = 0; return *this; }
+
+        if(n >= hsz) {
+            n -= hsz;
+            hi = lo;
+            lo = 0;
+        }
+
+        if(n != 0) {
+            hi <<= n;
+            const u64 mask(~(u64(-1) >> n));
+            hi |= (lo & mask) >> (hsz - n);
+            lo <<= n;
+        }
+
+        return *this;
+	}
+    U128 &operator>>=(const U128& rhs) {
+		size_t n = rhs.to_int();
+        if (n >= sz) { lo = hi = 0; return *this; }
+
+        if(n >= hsz) {
+            n -= hsz;
+            lo = hi;
+            hi = 0;
+        }
+
+        if(n != 0) {
+            lo >>= n;
+            const u64 mask(~(u64(-1) << n));
+            lo |= (hi & mask) << (hsz - n);
+            hi >>= n;
+        }
+
+        return *this;
+	}
+
+    int to_int() const { return static_cast<int>(lo); }
+    u64 to_u64() const { return lo; }
+
+    bool operator==(const U128 &b) const { return hi == b.hi && lo == b.lo; }
+    bool operator!=(const U128 &b) const { return !(*this == b); }
+    bool operator<(const U128 &b) const {
+        return (hi == b.hi) ? lo < b.lo : hi < b.hi; }
+    bool operator>=(const U128 &b) const { return ! (*this < b); }
+    U128 operator&(const U128 &b) const { U128 a(*this); return a &= b; }
+    U128 operator<<(const U128 &b) const { U128 a(*this); return a <<= b; }
+    U128 operator>>(const U128 &b) const { U128 a(*this); return a >>= b; }
+    U128 operator*(const U128 &b) const { U128 a(*this); return a *= b; }
+    U128 operator+(const U128 &b) const { U128 a(*this); return a += b; }
+    U128 operator-(const U128 &b) const { U128 a(*this); return a -= b; }
+    U128 operator%(const U128 &b) const { U128 a(*this); return a %= b; }
 };
 
 //
 // Fraction
 //
+template<typename T>
 struct Fraction {
-    int p, q;
+    T p, q;
 
-    Fraction(int P, int Q) : p(P), q(Q) { simplify(); }
-    Fraction(int P) : p(P), q(1) {}
+    Fraction() { p = 0, q = 1; }
+    Fraction(T P, T Q) : p(P), q(Q) { simplify(); }
     void simplify() {
-        int g = gcd(p, q);
+        T g = gcd(p, q);
         p /= g;
         q /= g;
     }

ef03dbb0193dd920ef771f1f320b21f8681acf9e
Leonardo Boshell <leonardo@diptongonante.com>
2013-02-04 05:53:37 -0500

    Little refactoring of geometry code

--- code/code.cpp
+++ code/code.cpp
@@ -102,7 +102,7 @@ struct Point {
     T x, y;
     Point() { x=y=0; }
     Point(T X, T Y) : x(X), y(Y) {}
-    T distance(const Point &p) {
+    T distance(const Point &p) const {
         T dx = p.x - x, dy = p.y - y; return sqrt(dx*dx + dy*dy);
     }
     bool operator<(const Point &p) const {
@@ -131,11 +131,36 @@ struct Vector {
         return fabs((p.x-a.x)*y - (p.y-a.y)*x) / sqrt(x*x + y*y);
     }
 };
-typedef vector<Point> PV;
+
+template <typename T>
 struct Circle {
-    double x, y, r;
+    T x, y, r;
     Circle() {}
-    Circle(double X, double Y, double R) : x(X), y(Y), r(R) {}
+    Circle(T X, T Y, T R) : x(X), y(Y), r(R) {}
+    // Finds the circle formed by three points
+    Circle(const Point<T> &p1, const Point<T> &p2, const Point<T> &p3) {
+        Point<T> m, a, b;
+        if (! eps_equal(p1.x, p2.x) && ! eps_equal(p1.x, p3.x))
+            m = p1, a = p2, b = p3;
+        else if (! eps_equal(p2.x, p1.x) && ! eps_equal(p2.x, p3.x))
+            m = p2, a = p1, b = p3;
+        else
+            m = p3, a = p1, b = p2;
+
+        T ma = (m.y - a.y) / (m.x - a.x);
+        T mb = (b.y - m.y) / (b.x - m.x);
+
+        x = ma*mb*(a.y - b.y) + mb*(a.x + m.x) - ma*(m.x + b.x);
+        x /= (mb - ma)*2.0;
+
+        if (eps_equal(0.0, ma))
+            y = (m.y + b.y) / 2.0 - (x - (m.x + b.x)/2.0) / mb;
+        else
+            y = (a.y + m.y) / 2.0 - (x - (a.x + m.x)/2.0) / ma;
+
+        r = p1.distance(Point<T>(x, y));
+    }
+    T circum() { return pi * r * 2; }
     bool perimeters_touch(const Circle &c) const {
         double dx = x - c.x;
         double dy = y - c.y;
@@ -180,30 +205,6 @@ void convex_hull(PV &p, PV &h) {
         h[k++] = p[i];
     }
     h.resize(k);
-}
-// Finds the circle formed by three points
-void find_circle(Point &p1, Point &p2, Point &p3, Point &c, double &r)
-{
-    Point m, a, b;
-    if (! eps_equal(p1.x, p2.x) && ! eps_equal(p1.x, p3.x))
-        m = p1, a = p2, b = p3;
-    else if (! eps_equal(p2.x, p1.x) && ! eps_equal(p2.x, p3.x))
-        m = p2, a = p1, b = p3;
-    else
-        m = p3, a = p1, b = p2;
-
-    double ma = (m.y - a.y) / (m.x - a.x);
-    double mb = (b.y - m.y) / (b.x - m.x);
-
-    c.x = ma*mb*(a.y - b.y) + mb*(a.x + m.x) - ma*(m.x + b.x);
-    c.x /= (mb - ma)*2.0;
-
-    if (eps_equal(0.0, ma))
-        c.y = (m.y + b.y) / 2.0 - (c.x - (m.x + b.x)/2.0) / mb;
-    else
-        c.y = (a.y + m.y) / 2.0 - (c.x - (a.x + m.x)/2.0) / ma;
-
-    r = c.distance(p1);
 }
 
 //

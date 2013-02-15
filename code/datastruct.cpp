8be038ee990fb3bbdbb7ee4e1fa71c39191f201a
Leonardo Boshell <leonardo@diptongonante.com>
2013-02-14 23:36:50 -0500

    Modified Trie code to use standard array

--- code/datastruct.cpp
+++ code/datastruct.cpp
@@ -3,25 +3,21 @@
 //
 struct Trie {
     struct Node {
-        int ch[26];
+        int ch[8];
         int n;
-        Node() { n=0; Zero(ch); }
     };
+    Node nodes[MAX_NODES];
     int sz;
-    vector<Node> nodes;
-    void init() { nodes.clear(); nodes.resize(1); sz = 1; }
-    void insert(const char *s) {
+    void init() { Zero(nodes); sz = 1; }
+    int insert(const char *s) {
         int idx, cur = 0;
-
         for (; *s; ++s) {
-            idx = *s - 'A';
-            if (! nodes[cur].ch[idx]) {
+            idx = toupper(s[i]) - 'A';
+            if (! nodes[cur].ch[idx])
                 nodes[cur].ch[idx] = sz++;
-                nodes.resize(sz);
-            }
-
             cur = nodes[cur].ch[idx];
         }
+        return ++nodes[cur].n;
     }
 };
 

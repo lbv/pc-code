02138438cae7078569d59b86a14b129ad95904f8
Leonardo Boshell <leonardo@diptongonante.com>
2013-03-05 12:58:10 -0500

    Added cases

--- solved/equalizing-money/gen.cpp
+++ solved/equalizing-money/gen.cpp
@@ -5,24 +5,41 @@
 #include <ctime>
 using namespace std;
 
-#define Zero(v) memset(v, 0, sizeof(v))
 
-#define MAXN 1000
-#define MAXM 10000
-
+#if 0
+#define MAXT     100
+#define MAXN     1000
+#define MAXM     10000
 #define MAXMONEY 1000
+#define NL       0
+#endif
+
+#if 1
+#define MAXT     20
+#define MAXN     8
+#define MAXM     12
+#define MAXMONEY 100
+#define NL       1
+#endif
+
+
+#define Zero(v) memset(v, 0, sizeof(v))
 
 bool rel[MAXN + 1][MAXN + 1];
 
-void test_case()
+void test_case(bool crit = false)
 {
-    int n = rand() % (MAXN - 1) + 2;
-    int m = rand() % min(MAXM, n*(n-1)/2);
+#if NL
+	puts("");
+#endif
+
+    int n = crit ? MAXN : rand() % (MAXN - 1) + 2;
+    int m = crit ? MAXM : rand() % (min(MAXM, n*(n-1)/2) + 1);
 
     printf("%d %d\n", n, m);
 
     for (int i = 0; i < n; ++i) {
-        int mon = rand() % MAXMONEY;
+        int mon = rand() % (MAXMONEY + 1);
         printf("%s%d", i == 0 ? "" : " ", mon);
     }
     putchar('\n');
@@ -34,11 +51,9 @@ void test_case()
         do {
             u = rand() % n + 1;
             v = rand() % n + 1;
-
-            if (v < u) swap(u, v);
         } while (u == v || rel[u][v]);
 
-        rel[u][v] = true;
+        rel[u][v] = rel[v][u] = true;
         printf("%d %d\n", u, v);
     }
 }
@@ -47,11 +62,11 @@ int main()
 {
     srand(time(NULL));
 
-    int T = 100;
+    int T = MAXT;
     printf("%d\n", T);
 
-    while (T--)
-        test_case();
+	test_case(true); --T;
+    while (T--) test_case();
 
     return 0;
 }

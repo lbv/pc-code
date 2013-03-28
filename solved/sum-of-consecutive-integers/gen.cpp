9d132f9c5ec7f3d2f57620487ce1ae17b486b075
Leonardo Boshell <leonardo@diptongonante.com>
2013-03-27 21:12:41 -0500

    Reimplemented generator, added cases

--- solved/sum-of-consecutive-integers/gen.cpp
+++ solved/sum-of-consecutive-integers/gen.cpp
@@ -1,95 +1,55 @@
-/*
-	Author       :	Jan
-	Problem Name :
-	Algorithm    :
-	Complexity   :
-*/
-
-#include <set>
-#include <map>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <cctype>
 #include <cstdio>
-#include <string>
-#include <vector>
-#include <cassert>
 #include <cstdlib>
-#include <cstring>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
+#include <ctime>
 
-using namespace std;
 
-typedef long long i64;
+#if 0
+#define MAXT 200
+#define MAXN 100000000000000LL
+#define PRE  1
+#endif
 
-#define PrimeLIMIT 10000010
-#define MAX_TOTAL 665000
+#if 1
+#define MAXT 200
+#define MAXN 100000000000000LL
+#define PRE  0
+#endif
 
-unsigned int prime[PrimeLIMIT / 64], pr[MAX_TOTAL], prlen;
-#define gP(n) (prime[n>>6]&(1<<((n>>1)&31)))
-#define rP(n) (prime[n>>6]&=~(1<<((n>>1)&31)))
-void sieve() {
-	unsigned int i,j,sqrtN,i2;
-	memset( prime, -1, sizeof( prime ) );
-	sqrtN = ( unsigned int ) sqrt ( ( double ) PrimeLIMIT ) + 1;
-	for( i = 3; i < sqrtN; i += 2 ) if( gP( i ) )
-		for( j = i * i, i2 = i << 1; j < PrimeLIMIT; j += i2 ) rP( j );
-	pr[prlen++] = 2;
-	for( i = 3; i < PrimeLIMIT; i += 2 ) if( gP( i ) ) pr[prlen++] = i;
+
+typedef unsigned long long u64;
+
+
+u64 rand_u64()
+{
+    u64 r = rand();
+    r <<= 32;
+    r |= rand();
+    return r;
 }
 
-int cases, caseno;
-i64 n;
+void test_case()
+{
+    u64 N = rand_u64() % MAXN + 1;
+    if (rand() % 20 == 0) N = rand() % 1000 + 1;
 
-i64 LIM = 100000000000000LL;
-
-int main() {
-	freopen("a.in", "w", stdout);
-
-	double cl = clock();
-
-	sieve();
-	int cases = 200;
-	printf("%d\n", cases);
-
-	for( int i = 0; i < 50; i++ ) {
-		printf("%lld\n", LIM - i);
-		cases--;
-	}
-	for( int i = 0; i < 50; i++ ) {
-		printf("%d\n", i + 1);
-		cases--;
-	}
-
-	while( cases-- ) {
-		i64 n = 1;
-
-		int it = rand() % 100 + 1;
-
-		while( it-- ) {
-			i64 x;
-			if( cases % 5 <= 2 ) {
-				x = pr[rand() % 100];
-			}
-			else {
-				x = pr[rand() % prlen];
-			}
-
-			if( n > 1000000000 && x > 100000 ) continue;
-			if( n * x <= LIM ) n *= x;
-		}
-		printf("%lld\n", n);
-	}
-
-	cl = clock() - cl;
-	fprintf(stderr, "Total Execution Time = %lf seconds\n", cl / CLOCKS_PER_SEC);
-
-	return 0;
+    printf("%llu\n", N);
 }
 
+int main()
+{
+    srand(time(NULL));
+
+    int T = MAXT;
+    printf("%d\n", T);
+
+#if PRE
+    for (int i = 0; i < 10; ++i) {
+        printf("%llu\n%llu\n", 1LL + i, MAXN - i);
+        T -= 2;
+    }
+#endif
+
+    while (T--) test_case();
+
+    return 0;
+}

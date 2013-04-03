54a653777d32dbcc0c5de77c23fb168d7a8f723b
Leonardo Boshell <leonardo@diptongonante.com>
2013-04-02 20:26:17 -0500

    Reimplemented generator, added cases

--- solved/one-way-roads/gen.cpp
+++ solved/one-way-roads/gen.cpp
@@ -1,49 +1,55 @@
+#include <algorithm>
 #include <cstdio>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <cctype>
 #include <cstdlib>
-
+#include <ctime>
 using namespace std;
 
-int main() {
-	// freopen("g.in", "w", stdout);
 
-	int cases = 195;
+#if 0
+#define MAXT 200
+#define MAXN 100
+#define MAXC 100
+#endif
 
-	printf("%d\n", cases);
-	while( cases-- ) {
-		int n;
+#if 1
+#define MAXT 20
+#define MAXN 12
+#define MAXC 100
+#endif
 
-		while(1) {
-			n = rand() % 101;
-			if( n >= 3 ) break;
-		}
-		if( cases == 30 ) n = 100;
-		if( cases == 10 ) n = 3;
 
-		int order[101];
-		for( int i = 0; i < n; i++ ) {
-			order[i] = i;
-		}
+int ring[MAXN];
+int edges[MAXN][3];
 
-		int x = rand() % 20000 + 10000;
-		while( x-- ) {
-			int p, q;
+void test_case(bool crit = false)
+{
+    int n = crit ? MAXN : rand() % (MAXN - 2) + 3;
+    printf("\n%d\n", n);
 
-			p = rand() % n;
-			q = rand() % n;
-			swap( order[p], order[q] );
-		}
-		printf("\n%d\n", n);
-		for( int i = 0; i < n; i++ ) {
-			int j = (i + 1) % n;
+    for (int i = 0; i < n; ++i) ring[i] = i + 1;
+    random_shuffle(ring, ring + n);
 
-			if( rand() % 2 ) printf("%d %d %d\n", order[i]+1, order[j]+1, rand() % 100 + 1);
-			else printf("%d %d %d\n", order[j]+1, order[i]+1, rand() % 100 + 1);
-		}
-	}
-	return 0;
+    for (int i = 0; i < n; ++i) {
+        edges[i][0] = ring[i];
+        edges[i][1] = ring[(i + 1) % n];
+        if (rand() % 2 == 0) swap(edges[i][0], edges[i][1]);
+        edges[i][2] = rand() % MAXC + 1;
+    }
+    random_shuffle(edges, edges + n);
+
+    for (int i = 0; i < n; ++i)
+        printf("%d %d %d\n", edges[i][0], edges[i][1], edges[i][2]);
 }
 
+int main()
+{
+    srand(time(NULL));
+
+    int T = MAXT;
+    printf("%d\n", T);
+
+    test_case(true); --T;
+    while (T--) test_case();
+
+    return 0;
+}

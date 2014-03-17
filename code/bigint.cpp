62ab164c23ab16a3f9161a7d2935d9618a60425d
Leonardo B <leonardo@diptongonante.com>
2014-03-17 01:15:45 -0500

    spaces -> tabs

--- code/bigint.cpp
+++ code/bigint.cpp
@@ -5,276 +5,276 @@
 #define BIDIG 3
 #define BIFMT "%03d"
 struct BigInt {
-    IV d; bool sgn;
-    BigInt(int n=0) {
-        if (n < 0) sgn = true, n = -n; else sgn = false;
-        if (n < BIBAS) d.push_back(n);
-        else while (n != 0) { d.push_back(n % BIBAS); n /= BIBAS; }
-    }
-    BigInt(const char *s) {
-        if (*s == '-') sgn = true, s++; else sgn = false;
-        for (int end = strlen(s), i = max(0, end-BIDIG); true;) {
-            int n = 0; for (int j=i; j != end; j++) n = n*10 + s[j] - '0';
-            d.push_back(n); if (i == 0) break;
-            end = i, i = max(0, i-BIDIG);
-        } clean();
-    }
-    size_t len() const { return d.size(); }
-    bool is_zero() const { return len() == 1 && d[0] == 0; }
-    void flip() { sgn = !sgn; }
-    BigInt neg() const { BigInt x = *this; x.flip(); return x; }
-    void clean() {
-        IVi i; for (i=d.end()-1; *i == 0 && i != d.begin(); i--);
-        d.erase(i+1, d.end());
-        if (sgn && is_zero()) sgn = false;
-    }
-    bool operator==(const BigInt &b) const {
-        return sgn == b.sgn && d == b.d;
-    }
-    bool operator<(const BigInt &b) const {
-        if (sgn != b.sgn) return sgn;
-        if (len() != b.len()) return sgn ^ (len() < b.len());
-        for (int i = len() - 1; i >= 0; --i)
-            if (d[i] != b.d[i]) return sgn ^ (d[i] < b.d[i]);
-        return false;
-    }
-    BigInt &operator*=(const BigInt &b) {
-        int s1 = len(), s2 = b.len(), s3 = s1+s2;
-        IV res(s3); int c = 0;
-        for (int k=0; k < s3; ++k) {
-            int sum = c;
-            for (int i=max(0,k-s2+1), I=min(k+1, s1), j=k-i; i < I; ++i, --j)
-                sum += d[i] * b.d[j];
-            if (sum >= BIBAS) { c = sum / BIBAS; sum %= BIBAS; } else c = 0;
-            res[k] = sum;
-        }
-        d = res; sgn ^= b.sgn; clean();
-        return *this;
-    }
-    BigInt &operator+=(const BigInt &b) {
-        if (sgn != b.sgn) { (*this) -= b.neg(); return *this; }
-        int s1 = len(), s2 = b.len(), s3 = max(s1, s2) + 1;
-        IV res(s3); int c = 0;
-        for (int i = 0; i < s3; ++i) {
-            int sum = c;
-            sum += i < s1 ? d[i] : 0;
-            sum += i < s2 ? b.d[i] : 0;
-            if (sum >= BIBAS) { c = sum / BIBAS; sum %= BIBAS; } else c = 0;
-            res[i] = sum;
-        }
-        d = res; clean();
-        return *this;
-    }
-    BigInt &operator-=(const BigInt &_b) {
-        if (sgn != _b.sgn) { (*this) += _b.neg(); return *this; }
-        bool sbk = sgn; sgn = false; BigInt b = _b.sgn ? _b.neg() : _b;
-        if (*this < b) {
-            b -= *this; *this = sbk ? b : b.neg(); return *this; }
-        int s1 = len(), s2 = b.len(), s3 = s1;
-        IV res(s3); int c = 0;
-        for (int i = 0; i < s3; ++i) {
-            int sum = d[i] - (i < s2 ? b.d[i] : 0) - c;
-            if (sum < 0) { sum += BIBAS; c = 1; } else c = 0;
-            res[i] = sum;
-        }
-        d = res; sgn = sbk; clean();
-        return *this;
-    }
-    BigInt &short_div(int b) {
-        for (int r = 0, i = len() - 1; i >= 0; --i)
-            r = r*BIBAS + d[i], d[i] = r / b, r %= b;
-        clean(); return *this;
-    }
-    BigInt &operator/=(const BigInt &b) {
-        if (b.is_zero()) { int x=0; return *this=BigInt(x/x); }
-        sgn ^= b.sgn; size_t l = len(), n = b.len();
-        if (n == 1) return short_div(b.d[0]);
-        if (l < n || (l == n && d.back() < b.d.back()))
-            return *this = BigInt(0);
-        BigInt r(0); IV res(l);
-        for (int i = l - 1; i >= 0; --i) {
-            r.d.insert(r.d.begin(), d[i]); r.clean();
-            int x = r.len() >= n ? r.d[n-1] : 0;
-            if (r.len() > n) x += BIBAS * r.d[n];
-            int q = x / b.d[n-1];
-            BigInt g = b; g *= BigInt(q);
-            while (r < g) g -= b, --q;
-            res[i] = q, r -= g;
-        }
-        d = res; clean(); return *this;
-    }
-    void print(bool nl = true) {
-        if (sgn) putchar('-');
-        bool flg = true;
-        RFor (IV, i, d) {
-            if (flg) { printf("%d", *i); flg=false; }
-            else printf(BIFMT, *i);
-        }
-        if (nl) putchar('\n');
-    }
-    string to_string() {
-        char buf[BIDIG+1]; string str;
-        if (sgn) str.push_back('-');
-        bool flg = true;
-        RFor (IV, i, d) {
-            if (flg) { sprintf(buf, "%d", *i); flg=false; }
-            else sprintf(buf, BIFMT, *i);
-            str.append(buf);
-        }
-        return str;
-    }
-    int to_int() {
-        int res = 0, p = 1;
-        for (int i=0, I=len(); i < I; i++) { res += d[i] * p; p *= BIBAS; }
-        return sgn ? -res : res;
-    }
-    BigInt operator+(const BigInt x) const {
-        BigInt ans = *this; ans += x; return ans; }
-    BigInt operator-(const BigInt x) const {
-        BigInt ans = *this; ans -= x; return ans; }
-    BigInt operator*(const BigInt x) const {
-        BigInt ans = *this; ans *= x; return ans; }
-    BigInt operator/(const BigInt x) const {
-        BigInt ans = *this; ans /= x; return ans; }
-    BigInt operator%(const BigInt x) const {
-        BigInt ans = *this; ans %= x; return ans; }
+	IV d; bool sgn;
+	BigInt(int n=0) {
+		if (n < 0) sgn = true, n = -n; else sgn = false;
+		if (n < BIBAS) d.push_back(n);
+		else while (n != 0) { d.push_back(n % BIBAS); n /= BIBAS; }
+	}
+	BigInt(const char *s) {
+		if (*s == '-') sgn = true, s++; else sgn = false;
+		for (int end = strlen(s), i = max(0, end-BIDIG); true;) {
+			int n = 0; for (int j=i; j != end; j++) n = n*10 + s[j] - '0';
+			d.push_back(n); if (i == 0) break;
+			end = i, i = max(0, i-BIDIG);
+		} clean();
+	}
+	size_t len() const { return d.size(); }
+	bool is_zero() const { return len() == 1 && d[0] == 0; }
+	void flip() { sgn = !sgn; }
+	BigInt neg() const { BigInt x = *this; x.flip(); return x; }
+	void clean() {
+		IVi i; for (i=d.end()-1; *i == 0 && i != d.begin(); i--);
+		d.erase(i+1, d.end());
+		if (sgn && is_zero()) sgn = false;
+	}
+	bool operator==(const BigInt &b) const {
+		return sgn == b.sgn && d == b.d;
+	}
+	bool operator<(const BigInt &b) const {
+		if (sgn != b.sgn) return sgn;
+		if (len() != b.len()) return sgn ^ (len() < b.len());
+		for (int i = len() - 1; i >= 0; --i)
+			if (d[i] != b.d[i]) return sgn ^ (d[i] < b.d[i]);
+		return false;
+	}
+	BigInt &operator*=(const BigInt &b) {
+		int s1 = len(), s2 = b.len(), s3 = s1+s2;
+		IV res(s3); int c = 0;
+		for (int k=0; k < s3; ++k) {
+			int sum = c;
+			for (int i=max(0,k-s2+1), I=min(k+1, s1), j=k-i; i < I; ++i, --j)
+				sum += d[i] * b.d[j];
+			if (sum >= BIBAS) { c = sum / BIBAS; sum %= BIBAS; } else c = 0;
+			res[k] = sum;
+		}
+		d = res; sgn ^= b.sgn; clean();
+		return *this;
+	}
+	BigInt &operator+=(const BigInt &b) {
+		if (sgn != b.sgn) { (*this) -= b.neg(); return *this; }
+		int s1 = len(), s2 = b.len(), s3 = max(s1, s2) + 1;
+		IV res(s3); int c = 0;
+		for (int i = 0; i < s3; ++i) {
+			int sum = c;
+			sum += i < s1 ? d[i] : 0;
+			sum += i < s2 ? b.d[i] : 0;
+			if (sum >= BIBAS) { c = sum / BIBAS; sum %= BIBAS; } else c = 0;
+			res[i] = sum;
+		}
+		d = res; clean();
+		return *this;
+	}
+	BigInt &operator-=(const BigInt &_b) {
+		if (sgn != _b.sgn) { (*this) += _b.neg(); return *this; }
+		bool sbk = sgn; sgn = false; BigInt b = _b.sgn ? _b.neg() : _b;
+		if (*this < b) {
+			b -= *this; *this = sbk ? b : b.neg(); return *this; }
+		int s1 = len(), s2 = b.len(), s3 = s1;
+		IV res(s3); int c = 0;
+		for (int i = 0; i < s3; ++i) {
+			int sum = d[i] - (i < s2 ? b.d[i] : 0) - c;
+			if (sum < 0) { sum += BIBAS; c = 1; } else c = 0;
+			res[i] = sum;
+		}
+		d = res; sgn = sbk; clean();
+		return *this;
+	}
+	BigInt &short_div(int b) {
+		for (int r = 0, i = len() - 1; i >= 0; --i)
+			r = r*BIBAS + d[i], d[i] = r / b, r %= b;
+		clean(); return *this;
+	}
+	BigInt &operator/=(const BigInt &b) {
+		if (b.is_zero()) { int x=0; return *this=BigInt(x/x); }
+		sgn ^= b.sgn; size_t l = len(), n = b.len();
+		if (n == 1) return short_div(b.d[0]);
+		if (l < n || (l == n && d.back() < b.d.back()))
+			return *this = BigInt(0);
+		BigInt r(0); IV res(l);
+		for (int i = l - 1; i >= 0; --i) {
+			r.d.insert(r.d.begin(), d[i]); r.clean();
+			int x = r.len() >= n ? r.d[n-1] : 0;
+			if (r.len() > n) x += BIBAS * r.d[n];
+			int q = x / b.d[n-1];
+			BigInt g = b; g *= BigInt(q);
+			while (r < g) g -= b, --q;
+			res[i] = q, r -= g;
+		}
+		d = res; clean(); return *this;
+	}
+	void print(bool nl = true) {
+		if (sgn) putchar('-');
+		bool flg = true;
+		RFor (IV, i, d) {
+			if (flg) { printf("%d", *i); flg=false; }
+			else printf(BIFMT, *i);
+		}
+		if (nl) putchar('\n');
+	}
+	string to_string() {
+		char buf[BIDIG+1]; string str;
+		if (sgn) str.push_back('-');
+		bool flg = true;
+		RFor (IV, i, d) {
+			if (flg) { sprintf(buf, "%d", *i); flg=false; }
+			else sprintf(buf, BIFMT, *i);
+			str.append(buf);
+		}
+		return str;
+	}
+	int to_int() {
+		int res = 0, p = 1;
+		for (int i=0, I=len(); i < I; i++) { res += d[i] * p; p *= BIBAS; }
+		return sgn ? -res : res;
+	}
+	BigInt operator+(const BigInt x) const {
+		BigInt ans = *this; ans += x; return ans; }
+	BigInt operator-(const BigInt x) const {
+		BigInt ans = *this; ans -= x; return ans; }
+	BigInt operator*(const BigInt x) const {
+		BigInt ans = *this; ans *= x; return ans; }
+	BigInt operator/(const BigInt x) const {
+		BigInt ans = *this; ans /= x; return ans; }
+	BigInt operator%(const BigInt x) const {
+		BigInt ans = *this; ans %= x; return ans; }
 };
 
 //
 // 128-bit unsigned integer
 //
 struct U128 {
-    u64 lo, hi;
-    static const size_t sz  = 128;
-    static const size_t hsz = 64;
-    static const u64 bmax = -1;
+	u64 lo, hi;
+	static const size_t sz	= 128;
+	static const size_t hsz = 64;
+	static const u64 bmax = -1;
 
-    U128() : lo(0), hi(0) {}
-    U128(int v) : lo(v), hi(0) {}
+	U128() : lo(0), hi(0) {}
+	U128(int v) : lo(v), hi(0) {}
 
-    U128 operator-() const { return ~U128(*this) + 1; }
-    U128 operator~() const {
+	U128 operator-() const { return ~U128(*this) + 1; }
+	U128 operator~() const {
 		U128 t(*this);
 		t.lo = ~t.lo;
 		t.hi = ~t.hi;
 		return t;
 	}
 
-    U128 &operator+=(const U128 &b) {
-        if (lo > bmax - b.lo) ++hi;
-        lo += b.lo;
-        hi += b.hi;
-        return *this;
-    }
-    U128 &operator-=(const U128 &b) { return *this += -b; }
-    U128 &operator*=(const U128 &b) {
-        if (*this == 0 || b == 1) return *this;
-        if (b == 0) { lo = hi = 0; return *this; }
+	U128 &operator+=(const U128 &b) {
+		if (lo > bmax - b.lo) ++hi;
+		lo += b.lo;
+		hi += b.hi;
+		return *this;
+	}
+	U128 &operator-=(const U128 &b) { return *this += -b; }
+	U128 &operator*=(const U128 &b) {
+		if (*this == 0 || b == 1) return *this;
+		if (b == 0) { lo = hi = 0; return *this; }
 
-        U128 a(*this);
-        U128 t = b;
+		U128 a(*this);
+		U128 t = b;
 
-        lo = hi = 0;
+		lo = hi = 0;
 
-        for (size_t i = 0; i < sz; ++i) {
-            if((t & 1) != 0) *this += (a << i);
-            t >>= 1;
-        }
-        return *this;
-    }
-    U128 &operator/=(const U128 &b) {
+		for (size_t i = 0; i < sz; ++i) {
+			if((t & 1) != 0) *this += (a << i);
+			t >>= 1;
+		}
+		return *this;
+	}
+	U128 &operator/=(const U128 &b) {
 		U128 rem;
 		divide(*this, b, *this, rem);
 		return *this;
-    }
-    U128 &operator%=(const U128 &b) {
+	}
+	U128 &operator%=(const U128 &b) {
 		U128 quo;
 		divide(*this, b, quo, *this);
 		return *this;
-    }
-
-    static void
-    divide(const U128 &num, const U128 &den, U128 &quo, U128 &rem) {
-        if(den == 0) { int a = 0; quo = U128(a/a); }
-        U128 n = num, d = den, x = 1, ans = 0;
-
-        while((n >= d) && (((d >> (sz - 1)) & 1) == 0)) {
-            x <<= 1;
-            d <<= 1;
-        }
-
-        while(x != 0) {
-            if(n >= d) {
-                n -= d;
-                ans |= x;
-            }
-
-            x >>= 1;
-            d >>= 1;
-        }
-
-        quo = ans, rem = n;
-    }
-
-    U128 &operator&=(const U128 &b) { hi &= b.hi; lo &= b.lo; return *this; }
-    U128 &operator|=(const U128 &b) {
-        hi |= b.hi;
-        lo |= b.lo;
-        return *this;
-    }
-    U128 &operator<<=(const U128& rhs) {
-		size_t n = rhs.to_int();
-        if (n >= sz) { lo = hi = 0; return *this; }
-
-        if(n >= hsz) {
-            n -= hsz;
-            hi = lo;
-            lo = 0;
-        }
-
-        if(n != 0) {
-            hi <<= n;
-            const u64 mask(~(u64(-1) >> n));
-            hi |= (lo & mask) >> (hsz - n);
-            lo <<= n;
-        }
-
-        return *this;
-	}
-    U128 &operator>>=(const U128& rhs) {
-		size_t n = rhs.to_int();
-        if (n >= sz) { lo = hi = 0; return *this; }
-
-        if(n >= hsz) {
-            n -= hsz;
-            lo = hi;
-            hi = 0;
-        }
-
-        if(n != 0) {
-            lo >>= n;
-            const u64 mask(~(u64(-1) << n));
-            lo |= (hi & mask) << (hsz - n);
-            hi >>= n;
-        }
-
-        return *this;
 	}
 
-    int to_int() const { return static_cast<int>(lo); }
-    u64 to_u64() const { return lo; }
+	static void
+	divide(const U128 &num, const U128 &den, U128 &quo, U128 &rem) {
+		if(den == 0) { int a = 0; quo = U128(a/a); }
+		U128 n = num, d = den, x = 1, ans = 0;
 
-    bool operator==(const U128 &b) const { return hi == b.hi && lo == b.lo; }
-    bool operator!=(const U128 &b) const { return !(*this == b); }
-    bool operator<(const U128 &b) const {
-        return (hi == b.hi) ? lo < b.lo : hi < b.hi; }
-    bool operator>=(const U128 &b) const { return ! (*this < b); }
-    U128 operator&(const U128 &b) const { U128 a(*this); return a &= b; }
-    U128 operator<<(const U128 &b) const { U128 a(*this); return a <<= b; }
-    U128 operator>>(const U128 &b) const { U128 a(*this); return a >>= b; }
-    U128 operator*(const U128 &b) const { U128 a(*this); return a *= b; }
-    U128 operator+(const U128 &b) const { U128 a(*this); return a += b; }
-    U128 operator-(const U128 &b) const { U128 a(*this); return a -= b; }
-    U128 operator%(const U128 &b) const { U128 a(*this); return a %= b; }
+		while((n >= d) && (((d >> (sz - 1)) & 1) == 0)) {
+			x <<= 1;
+			d <<= 1;
+		}
+
+		while(x != 0) {
+			if(n >= d) {
+				n -= d;
+				ans |= x;
+			}
+
+			x >>= 1;
+			d >>= 1;
+		}
+
+		quo = ans, rem = n;
+	}
+
+	U128 &operator&=(const U128 &b) { hi &= b.hi; lo &= b.lo; return *this; }
+	U128 &operator|=(const U128 &b) {
+		hi |= b.hi;
+		lo |= b.lo;
+		return *this;
+	}
+	U128 &operator<<=(const U128& rhs) {
+		size_t n = rhs.to_int();
+		if (n >= sz) { lo = hi = 0; return *this; }
+
+		if(n >= hsz) {
+			n -= hsz;
+			hi = lo;
+			lo = 0;
+		}
+
+		if(n != 0) {
+			hi <<= n;
+			const u64 mask(~(u64(-1) >> n));
+			hi |= (lo & mask) >> (hsz - n);
+			lo <<= n;
+		}
+
+		return *this;
+	}
+	U128 &operator>>=(const U128& rhs) {
+		size_t n = rhs.to_int();
+		if (n >= sz) { lo = hi = 0; return *this; }
+
+		if(n >= hsz) {
+			n -= hsz;
+			lo = hi;
+			hi = 0;
+		}
+
+		if(n != 0) {
+			lo >>= n;
+			const u64 mask(~(u64(-1) << n));
+			lo |= (hi & mask) << (hsz - n);
+			hi >>= n;
+		}
+
+		return *this;
+	}
+
+	int to_int() const { return static_cast<int>(lo); }
+	u64 to_u64() const { return lo; }
+
+	bool operator==(const U128 &b) const { return hi == b.hi && lo == b.lo; }
+	bool operator!=(const U128 &b) const { return !(*this == b); }
+	bool operator<(const U128 &b) const {
+		return (hi == b.hi) ? lo < b.lo : hi < b.hi; }
+	bool operator>=(const U128 &b) const { return ! (*this < b); }
+	U128 operator&(const U128 &b) const { U128 a(*this); return a &= b; }
+	U128 operator<<(const U128 &b) const { U128 a(*this); return a <<= b; }
+	U128 operator>>(const U128 &b) const { U128 a(*this); return a >>= b; }
+	U128 operator*(const U128 &b) const { U128 a(*this); return a *= b; }
+	U128 operator+(const U128 &b) const { U128 a(*this); return a += b; }
+	U128 operator-(const U128 &b) const { U128 a(*this); return a -= b; }
+	U128 operator%(const U128 &b) const { U128 a(*this); return a %= b; }
 };
 
--- code/numtheory.cpp
+++ code/numtheory.cpp
@@ -3,99 +3,99 @@
 //
 #define IsComp(n)  (_c[n>>6]&(1<<((n>>1)&31)))
 #define SetComp(n) _c[n>>6]|=(1<<((n>>1)&31))
-const int MAXP   = 46341;  // sqrt(2^31)
-const int SQRP   =   216;  // sqrt(MAX)
-const int MAX_NP =  5435;  // 1.26 * MAXP/log(MAXP)
+const int MAXP	 = 46341;  // sqrt(2^31)
+const int SQRP	 =	 216;  // sqrt(MAX)
+const int MAX_NP =	5435;  // 1.26 * MAXP/log(MAXP)
 int _c[(MAXP>>6)+1];
 int primes[MAX_NP];
 int nprimes;
 void prime_sieve() {
-    for (int i = 3; i <= SQRP; i += 2)
-        if (!IsComp(i)) for (int j = i*i; j <= MAXP; j+=i+i) SetComp(j);
-    primes[nprimes++] = 2;
-    for (int i=3; i <= MAXP; i+=2) if (!IsComp(i)) primes[nprimes++] = i;
+	for (int i = 3; i <= SQRP; i += 2)
+		if (!IsComp(i)) for (int j = i*i; j <= MAXP; j+=i+i) SetComp(j);
+	primes[nprimes++] = 2;
+	for (int i=3; i <= MAXP; i+=2) if (!IsComp(i)) primes[nprimes++] = i;
 }
 bool is_prime(int n) {
-    if (n < 2 || (n > 2 && n % 2 == 0)) return false;
-    return ! IsComp(n);
+	if (n < 2 || (n > 2 && n % 2 == 0)) return false;
+	return ! IsComp(n);
 }
 
 // Finds prime numbers between a and b, using basic primes up to sqrt(b)
 // pre cond: a > 2 (handle that separately)
 void prime_seg_sieve(i64 a, i64 b, IV &seg_primes) {
-    BV pmap(b - a + 1, true);
-    i64 sqr_b = sqrt(b);
-    For (IV, pp, primes) {
-        int p = *pp;
-        if (p > sqr_b) break;
-        for (i64 j = (a+p-1)/p, v=(j==1?p+p:j*p); v <= b; v += p)
-            pmap[v-a] = false;
-    }
-    seg_primes.clear();
-    for (int i = a%2 ? 0 : 1, I = b - a + 1; i < I; i += 2)
-        if (pmap[i])
-            seg_primes.push_back(a + i);
+	BV pmap(b - a + 1, true);
+	i64 sqr_b = sqrt(b);
+	For (IV, pp, primes) {
+		int p = *pp;
+		if (p > sqr_b) break;
+		for (i64 j = (a+p-1)/p, v=(j==1?p+p:j*p); v <= b; v += p)
+			pmap[v-a] = false;
+	}
+	seg_primes.clear();
+	for (int i = a%2 ? 0 : 1, I = b - a + 1; i < I; i += 2)
+		if (pmap[i])
+			seg_primes.push_back(a + i);
 }
 
 void prime_factorize(int n, IIV &f) {
-    int sn = sqrt(n);
-    For (IV, p, primes) {
-        int prime = *p;
-        if (prime > sn) break; if (n % prime) continue;
-        int e = 0; for (; n % prime == 0; e++, n /= prime);
-        f.push_back(II(prime, e));
-        sn = sqrt(n);
-    }
-    if (n > 1) f.push_back(II(n, 1));
+	int sn = sqrt(n);
+	For (IV, p, primes) {
+		int prime = *p;
+		if (prime > sn) break; if (n % prime) continue;
+		int e = 0; for (; n % prime == 0; e++, n /= prime);
+		f.push_back(II(prime, e));
+		sn = sqrt(n);
+	}
+	if (n > 1) f.push_back(II(n, 1));
 }
 
 template <typename T>
 void divisors(T n, T *ds, int &s)
 {
-    s = 0;
-    ds[s++] = 1;
-    int sn = sqrt(n);
-    for (int i = 0; i < nprimes; ++i) {
-        int &p = primes[i];
-        if (p > sn) break;
-        if (n % p != 0) continue;
-        T q = 1; int t = s;
-        while (n % p == 0) {
-            q *= p, n /= p;
-            for (int j = 0; j < t; ++j) ds[s++] = ds[j] * q;
-        }
-        sn = sqrt(n);
-    }
-    if (n > 1) {
-        int t = s;
-        for (int j = 0; j < t; ++j) ds[s++] = ds[j] * n;
-    }
+	s = 0;
+	ds[s++] = 1;
+	int sn = sqrt(n);
+	for (int i = 0; i < nprimes; ++i) {
+		int &p = primes[i];
+		if (p > sn) break;
+		if (n % p != 0) continue;
+		T q = 1; int t = s;
+		while (n % p == 0) {
+			q *= p, n /= p;
+			for (int j = 0; j < t; ++j) ds[s++] = ds[j] * q;
+		}
+		sn = sqrt(n);
+	}
+	if (n > 1) {
+		int t = s;
+		for (int j = 0; j < t; ++j) ds[s++] = ds[j] * n;
+	}
 }
 
 void euler_phi(int a[], int N) {
-    for (int i = 1; i <= N; i++) a[i] = i;
-    for (int i = 2; i <= N; i += 2) a[i] = i/2;
-    for (int i = 3; i <= N; i += 2)
-        if (a[i] == i) {
-            a[i]--; for (int j = i+i; j <= N; j += i) a[j] -= a[j]/i;
-        }
+	for (int i = 1; i <= N; i++) a[i] = i;
+	for (int i = 2; i <= N; i += 2) a[i] = i/2;
+	for (int i = 3; i <= N; i += 2)
+		if (a[i] == i) {
+			a[i]--; for (int j = i+i; j <= N; j += i) a[j] -= a[j]/i;
+		}
 }
 
 template <typename TB, typename TE>
 TB mod_pow(TB b, TE e, TB m) {
-    TB res = 1; for (; e; e >>= 1, b = b*b%m) if (e & 1) res = res*b%m;
-    return res;
+	TB res = 1; for (; e; e >>= 1, b = b*b%m) if (e & 1) res = res*b%m;
+	return res;
 }
 
 template<typename TB, typename TE>
 TB sq_pow(TB b, TE e) {
-    TB res = 1; for (; e; e >>= 1, b *= b) if (e & 1) res *= b;
-    return res;
+	TB res = 1; for (; e; e >>= 1, b *= b) if (e & 1) res *= b;
+	return res;
 }
 
 template<typename TB, typename TE>
 void sq_pow(TB b, TE e, TB &r) {
-    for (; e; e >>= 1, b *= b) if (e & 1) r *= b;
+	for (; e; e >>= 1, b *= b) if (e & 1) r *= b;
 }
 
 template<typename T>
@@ -103,57 +103,58 @@ T gcd(T a, T b) { for (T c = a%b; c != 0; a=b,b=c,c=a%b); return b; }
 
 template<typename T>
 void ext_euclid(T a, T b, T &x, T &y, T &g) {
-    x = 0; y = 1; g = b;
-    T m, n, q, r;
-    for (T u=1, v=0; a != 0; g=a, a=r) {
-        q = g / a; r = g % a;
-        m = x-u*q; n = y-v*q;
-        x=u; y=v; u=m; v=n;
-    }
+	x = 0; y = 1; g = b;
+	T m, n, q, r;
+	for (T u=1, v=0; a != 0; g=a, a=r) {
+		q = g / a; r = g % a;
+		m = x-u*q; n = y-v*q;
+		x=u; y=v; u=m; v=n;
+	}
+}
+
+template<typename T>
+T mod_inv(T n, T m) {
+	T x, y, gcd;
+	ext_euclid(n, m, x, y, gcd);
+	if (gcd == 1) return x % m;
+	return 0;
 }
 
 template <typename T>
 T chinese_rem(int k, T *ns, T *as)
 {
-    T N = 1, x = 0, r, s, g;
-    for (int i = 0; i < k; ++i) N *= ns[i];
-    for (int i = 0; i < k; ++i) {
-        ext_euclid(ns[i], N/ns[i], r, s, g);
-        x += as[i]*s*(N/ns[i]);
-        x %= N;
-    }
-    if (x < 0) x += N;
-    return x;
-}
-
-int mod_inv(int n, int m) {
-    int x, y, gcd;
-    ext_euclid(n, m, x, y, gcd);
-    if (gcd == 1) return x % m;
-    return 0;
+	T N = 1, x = 0, r, s, g;
+	for (int i = 0; i < k; ++i) N *= ns[i];
+	for (int i = 0; i < k; ++i) {
+		ext_euclid(ns[i], N/ns[i], r, s, g);
+		x += as[i]*s*(N/ns[i]);
+		x %= N;
+	}
+	if (x < 0) x += N;
+	return x;
 }
 
 // Calculates the highest exponent of prime p that divides n!
 int pow_div_fact(int n, int p) {
-    int sd = 0; for (int N=n; N; N /= p) sd += N % p; return (n-sd)/(p-1);
+	int sd = 0; for (int N=n; N; N /= p) sd += N % p; return (n-sd)/(p-1);
 }
 
 // Binomial coefficients
 #define BCSIZE 1000
 int bc[BCSIZE + 1][BCSIZE + 1];
 void choose_table() {
-    for (int n = 1; n <= BCSIZE; n++) { bc[n][n] = 1; bc[n][1] = n; }
-    for (int n = 3; n <= BCSIZE; n++)
-        for (int k = 2; k < n; k++)
-            bc[n][k] = (bc[n-1][k-1] + bc[n-1][k]) % MOD;
+	for (int n = 1; n <= BCSIZE; n++) { bc[n][n] = 1; bc[n][1] = n; }
+	for (int n = 3; n <= BCSIZE; n++)
+		for (int k = 2; k < n; k++)
+			bc[n][k] = (bc[n-1][k-1] + bc[n-1][k]) % MOD;
 }
 
 int choose(int n, int k) {
-    if (k > n) return 0;
-    i64 r=1;
-    for (int d = 1; d <= k; d++) {
-        r *= n--;
-        r /= d;
-    }
-    return r;
+	if (k > n) return 0;
+	i64 r=1;
+	for (int d = 1; d <= k; d++) {
+		r *= n--;
+		r /= d;
+	}
+	return r;
 }

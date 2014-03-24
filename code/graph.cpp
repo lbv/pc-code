b78281d713391337403dbcc32f34eb6742b377ba
Leonardo B <leonardo@diptongonante.com>
2014-03-24 07:30:26 -0500

    Updated Hopcroft-Karp code

--- code/graph.cpp
+++ code/graph.cpp
@@ -252,63 +252,6 @@ struct Graph {
 					dag.add(vcomp[u], vcomp[e->v], 0);
 	}
 
-	//
-	// Hopcroft-Karp for bipartite matching.
-	//
-	// Receives the vertices in G1, and depends on vertex #0 being reserved
-	// as the NIL vertex
-	struct HK {
-		IV pair_g1, pair_g2, dist;
-		Graph &g;
-		IV &g1;
-		HK(Graph &G, IV &G1) : g(G), g1(G1) {
-			pair_g1 = IV(g.n); pair_g2 = IV(g.n); dist = IV(g.n); }
-		bool bfs() {
-			IQ q;
-			cFor (IV, v, g1) {
-				if (pair_g1[*v] == 0) {
-					dist[*v] = 0;
-					q.push(*v);
-				}
-				else
-					dist[*v] = INF;
-			}
-			dist[0] = INF;
-			while (! q.empty()) {
-				int v = q.front(); q.pop();
-				cFor (EL, e, g.adj[v]) {
-					int p = pair_g2[e->v];
-					if (dist[p] != INF) continue;
-					dist[p] = dist[v] + 1;
-					q.push(p);
-				}
-			}
-			return dist[0] != INF;
-		}
-		bool dfs(int v) {
-			if (v == 0) return true;
-			cFor (EL, e, g.adj[v]) {
-				int p = pair_g2[e->v];
-				if (dist[p] == dist[v] + 1 && dfs(p)) {
-					pair_g2[e->v] = v;
-					pair_g1[v] = e->v;
-					return true;
-				}
-			}
-			dist[v] = INF;
-			return false;
-		}
-	};
-	int hopcroft(IV &g1) {
-		HK hk(*this, g1);
-		int m = 0;
-		while (hk.bfs())
-			cFor (IV, v, g1)
-				if (hk.pair_g1[*v] == 0 && hk.dfs(*v))
-					++m;
-		return m;
-	}
-
 
 	// Articulations/bridges
 	int low[MAX_V], idx[MAX_V], cnt;
@@ -397,10 +340,55 @@ struct Graph {
 		Neg(adj);
 	}
 
-	void add_bipart_basic(int v1, int v2, int w) {
-		int u = 1 + v1;
-		int v = x + 1 + v2;
-		add_pair(u, v, w);
+
+	//
+	// Hopcroft-Karp for bipartite matching.
+	//
+	int match[MAX_VERT];
+	int dist[MAX_VERT];
+	int q[MAX_VERT];
+
+	bool bfs() {
+		int qf = 0, qb = 0;
+		for (int i = 1; i <= x; ++i)
+			if (match[i] == 0)
+				dist[i] = 0, q[qb++] = i;
+			else
+				dist[i] = INF;
+		dist[0] = INF;
+		while (qf < qb) {
+			int v = q[qf++];
+			for (int i = adj[v]; i >= 0; i = next[i]) {
+				Edge &e = edges[i];
+				int p = match[e.v];
+				if (dist[p] != INF) continue;
+				dist[p] = dist[v] + 1;
+				q[qb++] = p;
+			}
+		}
+		return dist[0] != INF;
+	}
+	bool dfs(int u) {
+		if (u == 0) return true;
+		for (int i = adj[u]; i >= 0; i = next[i]) {
+			Edge &e = edges[i];
+			int p = match[e.v];
+			if (dist[p] == dist[u] + 1 && dfs(p)) {
+				match[e.v] = u;
+				match[u] = e.v;
+				return true;
+			}
+		}
+		dist[u] = INF;
+		return false;
+	}
+	int max_matching() {
+		int m = 0;
+		Clr(match);
+		while (bfs())
+			for (int i = 1; i <= x; ++i)
+				if (match[i] == 0 && dfs(i)) ++m;
+		return m;
 	}
 
 
@@ -488,7 +476,9 @@ struct Graph {
 		InfRange(price + x + 1, x, int);
 	}
 	vod add_bipart(int v1, int v2, int w) {
-		add_bipart_basic(v1, v2, w);
+		int u = 1 + v1;
+		int v = x + 1 + v2;
+		add_pair(u, v, w);
 		price[v] = min(price[v], w);
 	}
 	void add_pair(int u, int v, int c) {

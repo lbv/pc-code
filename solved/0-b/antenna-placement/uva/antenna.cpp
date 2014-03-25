96e2bd971751d85a86292b13c0b3d37b8efe78ad
Leonardo B <leonardo@diptongonante.com>
2014-03-24 21:59:01 -0500

    Added problem: Antenna Placement

---
+++ solved/0-b/antenna-placement/uva/antenna.cpp
@@ -0,0 +1,153 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+
+#define MAXH 40
+#define MAXW 10
+#define INF  0x3f3f3f3f
+
+const int MAX_VERT = MAXH * MAXW * 2;
+const int MAX_EDGES = MAX_VERT * 4;
+
+#define Neg(m) memset(m, -1, sizeof(m))
+#define Clr(m) memset(m, 0, sizeof(m))
+
+
+struct Edge { int v; Edge() {} Edge(int V) : v(V) {} };
+
+struct Graph {
+	Edge edges[MAX_EDGES];
+	int next[MAX_EDGES];
+	int adj[MAX_VERT];
+	int n, m;
+	Graph() {}
+	void init(int N) { n = N, m = 0; Neg(adj); }
+
+	void add(int u, const Edge &e) { next[m]=adj[u], adj[u]=m, edges[m++]=e; }
+
+	int x, src, snk;
+	void init_bipart(int X) {
+		x = X;
+		n = 2*x + 2, src = 0, snk = 2*x + 1, m = 0;
+		Neg(adj);
+	}
+
+	void add_bipart(int v1, int v2) {
+		int u = 1 + v1;
+		int v = x + 1 + v2;
+		add(u, Edge(v));
+	}
+
+	//
+	// Hopcroft-Karp for bipartite matching.
+	//
+	int match[MAX_VERT];
+	int dist[MAX_VERT];
+	int q[MAX_VERT];
+
+	bool bfs() {
+		int qf = 0, qb = 0;
+		for (int i = 1; i <= x; ++i)
+			if (match[i] == 0)
+				dist[i] = 0, q[qb++] = i;
+			else
+				dist[i] = INF;
+		dist[0] = INF;
+		while (qf < qb) {
+			int v = q[qf++];
+			for (int i = adj[v]; i >= 0; i = next[i]) {
+				Edge &e = edges[i];
+				int p = match[e.v];
+				if (dist[p] != INF) continue;
+				dist[p] = dist[v] + 1;
+				q[qb++] = p;
+			}
+		}
+		return dist[0] != INF;
+	}
+	bool dfs(int u) {
+		if (u == 0) return true;
+		for (int i = adj[u]; i >= 0; i = next[i]) {
+			Edge &e = edges[i];
+			int p = match[e.v];
+			if (dist[p] == dist[u] + 1 && dfs(p)) {
+				match[e.v] = u;
+				match[u] = e.v;
+				return true;
+			}
+		}
+		dist[u] = INF;
+		return false;
+	}
+	int max_matching() {
+		int m = 0;
+		Clr(match);
+		while (bfs())
+			for (int i = 1; i <= x; ++i)
+				if (match[i] == 0 && dfs(i)) ++m;
+		return m;
+	}
+};
+
+int h, w;
+char grid[MAXH][MAXW + 1];
+int idx[MAXH][MAXW];
+
+Graph g;
+
+const int moves[4][2] = {
+	{ -1,  0 },
+	{  1,  0 },
+	{  0,  1 },
+	{  0, -1 }
+};
+
+bool is_valid(int r, int c)
+{
+	return 0 <= r && r < h && 0 <= c && c < w;
+}
+
+int solve()
+{
+	int x = 0, y = 0;
+
+	for (int i = 0; i < h; ++i)
+		for (int j = 0; j < w; ++j) {
+			if (grid[i][j] != '*') continue;
+
+			if ((i+j) % 2 == 0) idx[i][j] = x++;
+			else idx[i][j] = y++;
+		}
+
+	g.init_bipart(max(x, y));
+
+	for (int i = 0; i < h; ++i)
+		for (int j = 0; j < w; ++j) {
+			if (grid[i][j] != '*' || (i+j) % 2 != 0) continue;
+
+			for (int k = 0; k < 4; ++k) {
+				int r = i + moves[k][0];
+				int c = j + moves[k][1];
+				if (is_valid(r, c) && grid[r][c] == '*')
+					g.add_bipart(idx[i][j], idx[r][c]);
+			}
+		}
+
+	return x + y - g.max_matching();
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	while (n--) {
+		scanf("%d%d", &h, &w);
+		for (int i = 0; i < h; ++i) scanf("%s", grid[i]);
+		printf("%d\n", solve());
+	}
+
+	return 0;
+}

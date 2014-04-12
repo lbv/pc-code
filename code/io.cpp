3d8ce5b2f7efffdc32edc14c94119e0bc48675c1
Leonardo B <leonardo@diptongonante.com>
2014-04-11 22:16:18 -0500

    Improvements to IO and Parsing code

--- code/io.cpp
+++ code/io.cpp
@@ -7,14 +7,18 @@ struct Reader {
     void skip() { while (b >= 0 && b <= 32) read(); }
 
     u32 next_u32() {
-        u32 v = 0; for (skip(); b > 32; read()) v = 10*v+b-48; return v; }
-    void next(char *s) { for (skip(); b > 32; read()) *s++ = b; *s = 0; }
-    char next_char() { skip(); char c = b; read(); return c; }
+        u32 v=0; for (skip(); 48<=b&&b<=57; read()) v = 10*v+b-48; return v; }
     int next_int() {
         int v = 0; bool s = false;
         skip(); if (b == '-') { s = true; read(); }
-        for (; b > 32; read()) v = v*10 + b-48; return s ? -v : v; }
+        for (; 48<=b&&b<=57; read()) v = v*10 + b-48; return s ? -v : v; }
+    char next_char() { skip(); char c = b; read(); return c; }
+    void next(char *s) { for (skip(); b > 32; read()) *s++ = b; *s = 0; }
+
     bool has_next() { skip(); return b > 0; }
+    bool has_next_u32() { skip(); return 48 <= b && b <= 57; }
+    bool has_next_int() { skip(); return b == 45 || (48 <= b && b <= 57); }
+
     void skip_line() {
         for (; b != 10 && b != 13 && b >= 0; read());
         char p = b; read();
--- code/misc.cpp
+++ code/misc.cpp
@@ -334,65 +334,87 @@ int josephus(int n, int k)
 }
 
 
+//
+// Recursive Descent Parser
+//
 
-enum TType {
+/**
+ * Example Grammar:
+ *
+ *   tree ::= '(' ( | INT <tree> <tree> ) ')'
+ */
+enum TokenT {
 	NONE,
-	Tok1,
-	Tok2
+	PAREN_OPEN,
+	PAREN_CLOSE,
+	INT
 };
 
 struct Token {
-	int v;
-	TType t;
-	Token(TType T) : v(0), t(T) {}
-	Token(int V, TType T) : v(V), t(T) {}
+	TokenT t;
+	int n;
+	Token() {}
+	Token(TokenT T): t(T) {}
+	Token(TokenT T, int N): t(T), n(N) {}
 };
-typedef queue<Token> TQ;
 
-struct AST {
-	struct Node {
-		int lt, rt;
-		int c0, c1;  // coefficients: c1*x + c0
-		TType op;
-		Node(int C0, int C1) : lt(-1), rt(-1), c0(C0), c1(C1) {}
-		Node(TType O, int L, int R) : lt(L), rt(R), op(O) {}
-	};
-	vector<Node> nodes;
+struct Lexer {
+	Reader &rr;
+	bool has_tok;
+	Token tok;
+	Lexer(Reader &R): rr(R), has_tok(false) {}
 
-	int add(int c0, int c1) {
-		int idx = nodes.size();
-		nodes.push_back(Node(c0, c1));
-		return idx;
+	void read_token() {
+		if (rr.has_next_int())
+			tok = Token(INT, rr.next_int());
+		else if (rr.has_next()) {
+			char c = rr.next_char();
+			switch (c) {
+			case '(': tok = Token(PAREN_OPEN); break;
+			case ')': tok = Token(PAREN_CLOSE); break;
+			default: tok = Token(NONE);
+			}
+		}
+		else
+			tok = Token(NONE);
+
+		has_tok = true;
 	}
-	int add(TType op, int lt, int rt) {
-		int idx = nodes.size();
-		nodes.push_back(Node(op, lt, rt));
-		return idx;
-	}
+
+	TokenT peek() { if (! has_tok) read_token(); return tok.t; }
+	Token pop() { if (! has_tok) read_token(); has_tok = false; return tok; }
 };
+
+struct TreeNode {
+	bool empty, leaf, ok;
+	TreeNode(): empty(true), leaf(false), ok(false) {}
+};
+
 
 struct Parser {
-	TQ &in;
-	bool err;
-	Parser(TQ &In) : in(In) { err = false; }
+	Lexer lex;
+	Parser(Reader &R): lex(Lexer(R)) {}
 
-	TType in_peek() {
-		if (in.empty()) return NONE;
-		return in.front().t;
-	}
-	Token in_pop() {
-		if (in.empty()) return Token(NONE);
-		Token t = in.front();
-		in.pop();
-		return t;
-	}
+	TreeNode tree(int sum) {
+		lex.pop(); // must be a PAREN_OPEN
 
-	void main_rule() {
-		if (in_peek() == CONJUNCTION) {
-			in_pop();
-			np();
-			vp();
+		TreeNode ans;
+		if (lex.peek() == INT) {
+			ans.empty = false;
+			Token tok = lex.pop();
+
+			TreeNode lt = tree(sum - tok.n);
+			TreeNode rt = tree(sum - tok.n);
+
+			if (lt.empty && rt.empty) {
+				ans.leaf = true;
+				if (tok.n == sum) ans.ok = true;
+			}
+			else
+				ans.ok = lt.ok || rt.ok;
 		}
-		if (in_peek() != NONE) err = true;
+
+		lex.pop();  // must be a PAREN_CLOSE
+		return ans;
 	}
 };

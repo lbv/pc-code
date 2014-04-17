69ffc198b930e6fe3efa992a4d590ef96c3d35d1
Leonardo B <leonardo@diptongonante.com>
2014-04-17 13:05:17 -0500

    Added code for intersection between circle and polygon.

--- code/geometry.cpp
+++ code/geometry.cpp
@@ -42,17 +42,17 @@ GeomT triangle_opp_angle(GeomT a, GeomT b, GeomT c)
 struct Point {
 	GeomT x, y;
 	Point() {}
-	Point(GeomT X, GeomT Y) : x(X), y(Y) {}
+	Point(GeomT X, GeomT Y): x(X), y(Y) {}
 
 	void dbg() const { printf("P(%d, %d)", x, y); }
 
+	GeomT norm() const { return sqrt(x*x + y*y); }
+
 	GeomT distance(const Point &p) const {
-		GeomT dx = p.x - x, dy = p.y - y; return sqrt(dx*dx + dy*dy);
-	}
+		GeomT dx = p.x - x, dy = p.y - y; return sqrt(dx*dx + dy*dy); }
 
 	bool is_collinear(const Point &a, const Point &b) const {
-		return eqz(cross(a, b));
-	}
+		return eqz(cross(a, b)); }
 
 	bool in_box(const Point &a, const Point &b) const {
 		GeomT lox = min(a.x, b.x), hix = max(a.x, b.x);
@@ -62,22 +62,26 @@ struct Point {
 	}
 
 	bool is_between(const Point &a, const Point &b) const {
-		return is_collinear(a, b) && in_box(a, b);
+		return is_collinear(a, b) && in_box(a, b); }
+
+	// Returns a point between {this}-{p}, according to {r} (0: this, 1: p)
+	Point interpolate(const Point &p, GeomT r) const {
+		GeomT dx = p.x - x, dy = p.y - y;
+		return Point(x + r*dx, y + r*dy);
 	}
 
 	// cross product magnitude of {a}x{b}, relative to {this}
 	GeomT cross(const Point &a, const Point &b) const {
-		return (a.x-x)*(b.y-y) - (a.y-y)*(b.x-x);
-	}
+		return (a.x-x)*(b.y-y) - (a.y-y)*(b.x-x); }
 
 	// {v} cross {this}, as vectors
 	GeomT cross(const Point &v) const { return x * v.y  - y * v.x; }
 
-	GeomT norm() { return sqrt(x*x + y*y); }
+	// signed angle
+	GeomT sangle(const Point &p) const {
+		return atan2(x*p.y - y*p.x, x*p.x + y*p.y); }
 
-	GeomT angle(const Point &p) const {
-		return circle_angle(atan2(p.y, p.x) - atan2(y, x));
-	}
+	GeomT angle(const Point &p) const { return circle_angle(sangle(p)); }
 
 	void rotate(double a) {
 		GeomT px = x, py = y;
@@ -108,13 +112,13 @@ typedef Point Vector;
 
 struct Line {
 	GeomT a, b, c;	// ax + by + c = 0
-	Line(GeomT A, GeomT B, GeomT C) : a(A), b(B), c(C) {}
+	Line(GeomT A, GeomT B, GeomT C): a(A), b(B), c(C) {}
 
 	Line(const Point &p1, const Point &p2) {
 		if (p1.x == p2.x) { a = 1, b = 0, c = -p1.x; return; }
 		a = p1.y-p2.y; b = p2.x-p1.x; c = -a*p1.x - b*p1.y;
 	}
-	Line(const Point &p, T m) { a = -m; b = 1; c = m*p.x - p.y; }
+	Line(const Point &p, GeomT m) { a = -m; b = 1; c = m*p.x - p.y; }
 
 	bool is_parallel(const Line &l) const { return a * l.b == b * l.a; }
 	bool is_vertical() const { return b == 0; }
@@ -143,6 +147,9 @@ struct Segment {
 	Point a, b;
 	Segment(Point A, Point B) : a(A), b(B) {}
 
+	void dbg() const {
+		printf("Seg( "); a.dbg(); printf(","); b.dbg(); printf(" )"); }
+
 	bool intersection(const Segment &s, Point &p) const {
 		Line l1(a, b);
 		Line l2(s.a, s.b);
@@ -155,7 +162,8 @@ struct Segment {
 struct Circle {
 	GeomT x, y, r;
 	Circle() {}
-	Circle(GeomT X, GeomT Y, GeomT R) : x(X), y(Y), r(R) {}
+	Circle(GeomT X, GeomT Y, GeomT R): x(X), y(Y), r(R) {}
+
 	// Finds the circle formed by three points
 	Circle(const Point &p1, const Point &p2, const Point &p3) {
 		Point m, a, b;
@@ -179,7 +187,7 @@ struct Circle {
 
 		r = p1.distance(Point<T>(x, y));
 	}
-	GeomT circum() { return pi * r * 2; }
+	GeomT circum() const { return pi * r * 2; }
 	bool perimeters_touch(const Circle &c) const {
 		double dx = x - c.x;
 		double dy = y - c.y;
@@ -187,6 +195,7 @@ struct Circle {
 		return ! (eps_less(r + c.r, dist) ||
 				  eps_less(dist, fabs(r - c.r)));
 	}
+
 	void tangentPoints(const Point &p, Point &p1, Point &p2) {
 		GeomT pox = x - p.x;
 		GeomT poy = y - p.y;
@@ -194,6 +203,78 @@ struct Circle {
 		GeomT s = sqrt(h2 - r*r);
 		p1 = Point(p.x + s*(pox*s-poy*r)/h2, p.y + s*(poy*s+pox*r)/h2);
 		p2 = Point(p.x + s*(pox*s+poy*r)/h2, p.y + s*(poy*s-pox*r)/h2);
+	}
+
+	// Finds points of intersection with a line segment ab. If there is a
+	// point "entering", uset will be true and u will be set. If there is a
+	// point "exiting", vset will be true, and v will be set. Returns true
+	// if a and b are both inside the circle.
+	bool intersect_with_segment(
+		const Point &a, const Point &b, bool &uset, Point &u,
+		bool &vset, Point &v) const {
+		uset = vset = false;
+
+		GeomT A = (b.x-a.x) * (b.x-a.x) + (b.y - a.y) * (b.y - a.y);
+		GeomT B = ((b.x-a.x) * (a.x-x) + (b.y - a.y) * (a.y - y))*2;
+		GeomT C = x*x + y*y + a.x*a.x + a.y*a.y - 2*(x*a.x + y*a.y) - r*r;
+
+		GeomT disc = B*B - 4*A*C;
+		if (cmp(disc, 0) <= 0) return false;
+
+		GeomT e = sqrt(disc);
+		GeomT u1 = (-B + e) / (2*A);
+		GeomT u2 = (-B - e) / (2*A);
+
+		int u1_0 = cmp(u1, 0), u1_1 = cmp(u1, 1);
+		int u2_0 = cmp(u2, 0), u2_1 = cmp(u2, 1);
+
+		if ((u1_0 < 0 && u2_0 < 0) || (u1_1 > 0 && u2_1 > 0)) return false;
+		if ((u1_0 < 0 || u1_1 > 0) && (u2_0 < 0 || u2_1 > 0)) return true;
+
+		if (u2_0 >= 0 && u2_1 <= 0)
+			u = a.interpolate(b, u2), uset = true;
+		if (u1_0 >= 0 && u1_1 <= 0)
+			v = a.interpolate(b, u1), vset = true;
+
+		// if uset && vset && u == v, the segment is tangent
+		return false;
+	}
+
+	// (signed) area of intersection with a triangle anchored on the
+	// circle's center
+	GeomT area_with_atriangle(const Point &a, const Point &b) const {
+		Point u, v; bool us, vs;
+		bool inside = intersect_with_segment(a, b, us, u, vs, v);
+
+		Point c(x, y);
+		if (inside) return c.cross(a, b) / 2;
+		if (! us && ! vs) {
+			Vector av = a-c, bv = b-c;
+			return av.sangle(bv)/2 * r*r;
+		}
+		GeomT ans = 0;
+		if (us) {
+			Vector av = a-c, uv = u-c;
+			ans += av.sangle(uv)/2 * r*r;
+		}
+		else ans += c.cross(a, v) / 2;
+
+		if (vs) {
+			Vector vv = v-c, bv = b-c;
+			ans += vv.sangle(bv)/2 * r*r;
+		}
+		else ans += c.cross(u, b) / 2;
+
+		if (us && vs) ans += c.cross(u, v) / 2;
+		return ans;
+	}
+
+	// (signed) area of intersection with an arbitrary polygon
+	GeomT area_with_poly(Point *poly, int n) const {
+		GeomT a = 0;
+		for (int i = 0, j = n - 1; i < n; j = i++)
+			a += area_with_atriangle(poly[j], poly[i]);
+		return a;
 	}
 };
 
@@ -211,7 +292,7 @@ GeomT cross_sgn(const Point &a, const Point &b)
 }
 
 // area of a polygon, times two
-GeomT poly_area2(Point *p, int n)
+GeomT poly_area_t2(Point *p, int n)
 {
 	GeomT a = 0;
 	for (int i = 0, j = n - 1; i < n; j = i++)

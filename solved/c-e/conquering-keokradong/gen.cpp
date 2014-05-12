e9fd262eddcc3fca987ab9003ae8cb7c1ca4b072
Leonardo B <leonardo@diptongonante.com>
2014-05-12 07:08:36 -0500

    Added extra cases

--- solved/c-e/conquering-keokradong/gen.cpp
+++ solved/c-e/conquering-keokradong/gen.cpp
@@ -3,40 +3,44 @@
 #include <ctime>
 
 
-#if 1
+#if 0
 #define MAXT 200
 #define MAXN 1000
 #define MAXK 300
 #define MAXM 10000
 #endif
 
-#if 0
-#define MAXT 500
-#define MAXN 100
-#define MAXK 80
+#if 1
+#define MAXT 20
+#define MAXN 10
+#define MAXK 9
 #define MAXM 100
+#define NL 1
 #endif
 
 
 void test_case()
 {
-    int N = rand() % MAXN + 1;
-    int Lim = N < 300 ? N : 300;
-    int K = rand() % Lim + 1;
+	int N = rand() % MAXN + 1;
+	int Lim = N < MAXK ? N : MAXK;
+	int K = rand() % Lim + 1;
 
-    printf("%d %d\n", N, K);
-    for (int i = 0; i <= N; ++i)
-        printf("%d\n", rand() % MAXM + 1);
+#if NL
+	puts("");
+#endif
+	printf("%d %d\n", N, K);
+	for (int i = 0; i <= N; ++i)
+		printf("%d\n", rand() % MAXM + 1);
 }
 
 int main()
 {
-    srand(time(NULL));
+	srand(time(NULL));
 
-    int T = MAXT;
-    printf("%d\n", T);
+	int T = MAXT;
+	printf("%d\n", T);
 
-    while (T--) test_case();
+	while (T--) test_case();
 
-    return 0;
+	return 0;
 }

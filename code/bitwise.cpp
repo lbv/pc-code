01df86de90a8d4bc6be35a128d722686181d7f2c
Leonardo B <leonardo@diptongonante.com>
2014-05-21 15:04:21 -0500

    Edits to bitwise code

--- code/bitwise.cpp
+++ code/bitwise.cpp
@@ -1,13 +1,9 @@
-#define GetFS(b) ((b) & -(b))
-#define ClrFS(b) (b &= ~GetFS(b))
-// values of (1 << x) for x<32 are unique modulo 37
-static const int m37pos[] = {
-	32,  0,  1, 26,  2, 23, 27,  0,  3,
-	16, 24, 30, 28, 11,  0, 13,  4,  7,
-	17,  0, 25, 22, 31, 15, 29, 10, 12,
-	 6,  0, 21, 14,  9,  5, 20,  8, 19, 18
-};
-#define Ctz(x) (m37pos[(x) % 37])
+#define PosLSB(x) (__builtin_ctzll(x))
+#define GetLSB(x) ((x) & -(x))
+#define ClrLSB(x) (x &= ~GetLSB(x))
+
+#define PosMSB(x) (63 - __builtin_clzll(x))
+#define GetMSB(x) (1ULL << PosMSB(x))
 
 // Returns first integer with exactly n bits set
 u32 popcnt_init(int n) { return (1 << n) - 1; }
@@ -18,6 +14,20 @@ u32 popcnt_next(u32 n)
 	u32 c = (n & -n), r = n+c;
 	return (((r ^ n) >> 2) / c) | r;
 }
+
+
+//
+// Variants for non-gcc compilers
+//
+
+// values of (1 << x) for x<32 are unique modulo 37
+static const int m37pos[] = {
+	32,  0,  1, 26,  2, 23, 27,  0,  3,
+	16, 24, 30, 28, 11,  0, 13,  4,  7,
+	17,  0, 25, 22, 31, 15, 29, 10, 12,
+	 6,  0, 21, 14,  9,  5, 20,  8, 19, 18
+};
+#define PosLSB(x) (m37pos[(x) % 37])
 
 // Finds the most significant bit set on n. The bit is left in b, and its
 // zero-indexed position in p

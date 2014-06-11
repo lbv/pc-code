abe34e02857e31ec9f7da6889db910df8d49cb7f
Leonardo B <leonardo@diptongonante.com>
2014-06-10 20:13:07 -0500

    Improved generator, added cases

--- solved/0-b/an-easy-lcs/gen.cpp
+++ solved/0-b/an-easy-lcs/gen.cpp
@@ -3,33 +3,47 @@
 #include <ctime>
 
 
+#if 0
 #define MAXT 100
 #define MAXLEN 100
+#define NCRIT 10
+#endif
+
+#if 1
+#define MAXT 20
+#define MAXLEN 33
+#define NCRIT 3
+#endif
+
+
+int T;
 
 
 void test_case(bool crit = false)
 {
-    int len1 = crit ? MAXLEN : rand() % MAXLEN + 1;
-    int len2 = crit ? MAXLEN : rand() % MAXLEN + 1;
+	int len1 = crit ? MAXLEN : rand() % MAXLEN + 1;
+	int len2 = crit ? MAXLEN : rand() % MAXLEN + 1;
 
-    putchar('\n');
-    for (int i = 0; i < len1; ++i)
-        printf("%c", 'a' + (rand() % 26));
-    putchar('\n');
-    for (int i = 0; i < len2; ++i)
-        printf("%c", 'a' + (rand() % 26));
-    putchar('\n');
+	int alpha = rand() % 26 + 1;
+	putchar('\n');
+	for (int i = 0; i < len1; ++i)
+		printf("%c", 'a' + (rand() % alpha));
+	putchar('\n');
+	for (int i = 0; i < len2; ++i)
+		printf("%c", 'a' + (rand() % alpha));
+	putchar('\n');
+	--T;
 }
 
 int main()
 {
-    srand(time(NULL));
+	srand(time(NULL));
 
-    int T = MAXT;
-    printf("%d\n", T);
+	T = MAXT;
+	printf("%d\n", T);
 
-    test_case(true); --T;
-    while (T--) test_case();
+	for (int i = 0; i < NCRIT; ++i) test_case(true);
+	while (T) test_case();
 
-    return 0;
+	return 0;
 }

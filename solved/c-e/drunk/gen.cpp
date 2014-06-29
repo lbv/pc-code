20ecece236acf1d66945a269b2967ac33c76c3fc
Leonardo B <leonardo@diptongonante.com>
2014-06-28 21:28:28 -0500

    Reimplemented generator, added cases

--- solved/c-e/drunk/gen.cpp
+++ solved/c-e/drunk/gen.cpp
@@ -1,81 +1,97 @@
-/*
-	Author       :	Jan
-	Problem Name :
-	Algorithm    :
-	Complexity   :
-*/
-
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
 #include <algorithm>
-#include <iostream>
 #include <cstdio>
-#include <cmath>
 #include <cstdlib>
-#include <cctype>
-#include <string>
 #include <cstring>
-#include <sstream>
-
+#include <ctime>
 using namespace std;
 
-set <string> S;
-char names[30001][12];
 
-int main() {
-	// freopen("d.in","w",stdout);
+#if 0
+#define MAXT 50
+#define MAXN 1000
+#define MAXM 10000
+#define MAXLEN 10
+#define ALPHA 26
+#define NCRIT 3
+#endif
 
-	for( int i = 0; i < 30000; i++ ) {
-		int len = rand() % 10 + 1;
-		char a[20];
-		for( int j = 0; j < len; j++ ) {
-			a[j] = rand() % 26 + 97;
-		}
-		a[len] = 0;
-		S.insert(a);
-	}
-	set <string> ::iterator s;
-	int N = 0;
-	for( s = S.begin(); s != S.end(); s++ ) {
-		strcpy( names[N], (*s).c_str() );
-		N++;
-	}
+#if 1
+#define MAXT 20
+#define MAXN 10
+#define MAXM 20
+#define MAXLEN 5
+#define NCRIT 4
+#define ALPHA 6
+#define NL 1
+#endif
 
-	int cases = 50;
+#define Clr(m) memset(m, 0, sizeof(m))
 
-	printf("%d\n", cases);
 
-	while( cases-- ) {
-		int n = rand() % 10000 + 1;
+struct Drink {
+	char name[MAXLEN + 1];
 
-		if( cases % 10 == 0 ) n = 10000;
-		else n = rand() % 100 + 1;
+	bool operator<(const Drink &x) const { return strcmp(name, x.name) < 0; }
+	bool operator==(const Drink &x) const { return strcmp(name, x.name) == 0; }
+};
 
-		if( n < 5 ) n = 5;
 
-		printf("%d\n", n);
-		int k  = n;
-		while( n-- ) {
-			int a, b;
-			
-			while( 1 ) {
-				if( cases % 5 != 0 ) {
-					a = rand() % S.size();
-					b = rand() % S.size();
-				}
-				else {
-					a = rand() % (k/2);
-					b = rand() % (k/2);
-				}
-				if( a != b ) break;
-			}
-			printf("%s %s\n", names[a], names[b]);
-		}
-	}
-	return 0;
+int T;
+Drink drinks[MAXN];
+bool g[MAXN][MAXN];
+
+
+void gen_drink(int i)
+{
+	Drink &d = drinks[i];
+
+	int len = rand() % MAXLEN + 1;
+	for (int i = 0; i < len; ++i)
+		d.name[i] = 'a' + (rand() % ALPHA);
+	d.name[len] = 0;
 }
 
+void gen(bool crit = false)
+{
+#if NL
+	puts("");
+#endif
+
+	int n = crit ? MAXN : rand() % (MAXN - 1) + 2;
+
+	for (int i = 0; i < n; ++i) gen_drink(i);
+	while (drinks[0] == drinks[1]) gen_drink(0);
+	sort(drinks, drinks + n);
+	n = unique(drinks, drinks + n) - drinks;
+
+	int maxm = min(n*(n-1)/2, MAXM);
+	int m = crit ? maxm : rand() % maxm + 1;
+
+	printf("%d\n", m);
+	Clr(g);
+	while (m--) {
+		int u, v;
+		do {
+			u = rand() % n;
+			v = rand() % n;
+		} while (u == v || g[u][v]);
+		g[u][v] = g[v][u] = true;
+
+		printf("%s %s\n", drinks[u].name, drinks[v].name);
+	}
+
+	--T;
+}
+
+int main()
+{
+	srand(time(NULL));
+
+	T = MAXT;
+	printf("%d\n", T);
+
+	for (int i = 0; i < NCRIT; ++i) gen(true);
+	while (T) gen();
+
+	return 0;
+}

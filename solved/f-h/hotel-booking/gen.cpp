baba7434efd21bc970eb301f7fbe0a850ff9336d
Leonardo B <leonardo@diptongonante.com>
2014-07-07 19:04:27 -0500

    Improved generator, added cases

--- solved/f-h/hotel-booking/gen.cpp
+++ solved/f-h/hotel-booking/gen.cpp
@@ -1,51 +1,83 @@
 #include <algorithm>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <ctime>
 using namespace std;
 
-#define MAXCASES 10
 
+#if 0
+#define MAXCASES 10
 #define MAXN 10000
 #define MAXM 100000
 #define MAXT 600
+#define NCRIT 2
+#endif
 
-void test_case(bool crit = false)
+#if 1
+#define MAXCASES 10
+#define MAXN 10
+#define MAXM 50
+#define MAXT 600
+#define NL 1
+#define NCRIT 3
+#endif
+
+
+#define ClrN(m,n,t) memset(m, 0, sizeof(t)*(n))
+
+
+int T;
+
+bool hotels[MAXN + 1];
+
+
+void gen(bool crit = false)
 {
-    int n = crit ? MAXN : rand() % (MAXN - 1) + 2;
-    int h = rand() % (min(n, 100) + 1);
+#if NL
+	puts("");
+#endif
+	int n = crit ? MAXN : rand() % (MAXN - 1) + 2;
+	int maxh = min(n, 100);
+	int h = crit ? maxh : rand() % (maxh + 1);
 
-    printf("%d\n%d", n, h);
-    while (h--) {
-        int c = rand() % n + 1;
-        printf(" %d", c);
-    }
+	printf("%d\n%d", n, h);
+	ClrN(hotels, n + 1, bool);
+	while (h--) {
+		int c;
+		do {
+			c = rand() % n + 1;
+		} while (hotels[c]);
+		hotels[c] = true;
+		printf(" %d", c);
+	}
 
-    int m = crit ? MAXM : rand() % MAXM + 1;
-    printf("\n%d\n", m);
+	int m = crit ? MAXM : rand() % MAXM + 1;
+	printf("\n%d\n", m);
 
-    while (m--) {
-        int u, v;
-        do {
-            u = rand() % n + 1;
-            v = rand() % n + 1;
-        } while (u == v);
+	while (m--) {
+		int u, v;
+		do {
+			u = rand() % n + 1;
+			v = rand() % n + 1;
+		} while (u == v);
 
-        int t = rand() % MAXT + 1;
+		int t = rand() % MAXT + 1;
 
-        printf("%d %d %d\n", u, v, t);
-    }
+		printf("%d %d %d\n", u, v, t);
+	}
+	--T;
 }
 
 int main()
 {
-    srand(time(NULL));
+	srand(time(NULL));
 
-    int T = MAXCASES;
+	T = MAXCASES;
 
-    test_case(true); --T;
-    while (T--) test_case();
-    puts("0");
+	for (int i = 0; i < NCRIT; ++i) gen(true);
+	while (T) gen();
+	puts("0");
 
-    return 0;
+	return 0;
 }

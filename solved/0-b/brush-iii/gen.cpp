12ad98f4ac9d3a678b3b3d68056c00c3dd2436a5
Leonardo B <leonardo@diptongonante.com>
2014-07-10 03:02:10 -0500

    Improved generator, added case

--- solved/0-b/brush-iii/gen.cpp
+++ solved/0-b/brush-iii/gen.cpp
@@ -6,23 +6,25 @@ using namespace std;
 
 
 #if 0
-#define MAXT     100
-#define MAXN     100
-#define MAXW     10000
-#define MAXK     100
+#define MAXT	 100
+#define MAXN	 100
+#define MAXW	 10000
+#define MAXK	 100
 #define MINCOORD -1000000000
 #define MAXCOORD 1000000000
-#define USE_X    1
+#define USE_X	 1
+#define NCRIT 8
 #endif
 
 #if 1
-#define MAXT     20
-#define MAXN     15
-#define MAXW     200
-#define MAXK     12
+#define MAXT	 20
+#define MAXN	 15
+#define MAXW	 200
+#define MAXK	 12
 #define MINCOORD -1000
 #define MAXCOORD 1000
-#define USE_X    0
+#define USE_X	 1
+#define NCRIT 2
 #endif
 
 
@@ -35,53 +37,59 @@ typedef pair<int, int> II;
 typedef set<II> IIS;
 
 II pts[MAXN];
+int T;
 
 
-void test_case(bool crit = false)
+void gen(bool crit = false)
 {
-    int N = rand() % MAXN + 1;
-    int w = rand() % MAXW + 1;
-    int k = rand() % MAXK + 1;
+	int N = crit ? MAXN : rand() % MAXN + 1;
+	int w = rand() % MAXW + 1;
+	int k = rand() % MAXK + 1;
 
-    if (crit) N = MAXN;
+	if (crit) N = MAXN;
 
-    if (rand() % 5 != 0) k = rand() % N + 1;
+	if (rand() % 5 != 0) k = rand() % N + 1;
 
-    printf("\n%d %d %d\n", N, w, k);
+	printf("\n%d %d %d\n", N, w, k);
 
-    IIS s;
-    for (int i = 0; i < N; ++i) {
-        int x, y;
-        do {
+	IIS s;
+	for (int i = 0; i < N; ++i) {
+		int x, y;
+		do {
 #if USE_X
-            x = rand() % CoordRange + MINCOORD;
+			x = rand() % CoordRange + MINCOORD;
 #else
-            x = 0;
+			x = 0;
 #endif
-            y = rand() % CoordRange + MINCOORD;
+			y = rand() % CoordRange + MINCOORD;
 
-            if (rand() % 3 != 0 && i > 0) {
-                int idx = rand() % i;
-                int delta = rand() % (w + 2) + 1;
+			if (rand() % 3 != 0 && i > 0) {
+				int idx = rand() % i;
+				int delta = rand() % (w + 2) + 1;
+				int r = rand() % 3;
+				if (r == 0) delta = -delta;
+				else if (r == 1) delta = 0;
 
-                y = pts[idx].second + (rand() % 2 == 0 ? delta : -delta);
-            }
-        } while (y >= MINCOORD && y <= MAXCOORD && s.find(II(x, y)) != s.end());
+				y = pts[idx].second + delta;
+			}
+		} while (y >= MINCOORD && y <= MAXCOORD && s.find(II(x, y)) != s.end());
 
-        s.insert(II(x, y));
-        pts[i] = II(x, y);
-        printf("%d %d\n", x, y);
-    }
+		s.insert(II(x, y));
+		pts[i] = II(x, y);
+		printf("%d %d\n", x, y);
+	}
+	--T;
 }
 
 int main()
 {
-    srand(time(NULL));
+	srand(time(NULL));
 
-    int T = MAXT;
-    printf("%d\n", T);
+	T = MAXT;
+	printf("%d\n", T);
 
-    while (T--) test_case();
+	for (int i = 0; i < NCRIT; ++i) gen(true);
+	while (T) gen();
 
-    return 0;
+	return 0;
 }

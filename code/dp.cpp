f6179eaa0e8f8e1c11112a994a0ee7beae8c72fe
Leonardo B <leonardo@diptongonante.com>
2014-08-26 17:00:05 -0500

    Added CYK algorithm

--- code/dp.cpp
+++ code/dp.cpp
@@ -26,6 +26,7 @@ int lis(const LisT *seq, int n, LisT *ls)
 	return len;
 }
 
+
 //
 // Coin change
 //	 W:  answer
@@ -70,3 +71,47 @@ struct DPString {
 	}
 };
 DPString dps;
+
+
+//
+// CYK (Cocke-Younger-Kasami) algorithm for parsing context-free grammars
+//
+const int MaxN = MAXC;       // max length of input string
+const int MaxS = MAXCOLORS;  // max number of symbols
+
+struct Rule { int X, A, B; };  // X -> A B
+
+// CYK(i, j, k) is the interface to the DP table. Each cell is true if
+// symbol k produces the substring Si..Sj
+int CYK_N, CYK_S;
+#define CYK(i,j,k)       (_cyk_dp[(i)*CYK_N*CYK_S + (j)*CYK_S + (k)])
+#define CYK_AUX(i,j)     (_cyk_aux[(i)*CYK_N + (j)])
+#define CYK_SET(i, j, k) (CYK_AUX(i, j) = CYK(i, j, k) = true)
+bool _cyk_dp[MaxN * MaxN * MaxS];
+bool _cyk_aux[MaxN * MaxN];  // for optimization purposes
+
+// call with the length of the input string (N) and the number of symbols (S)
+void cyk_init(int N, int S)
+{
+	Clr(_cyk_dp);
+	Clr(_cyk_aux);
+	CYK_N = N, CYK_S = S;
+}
+
+// pre-cond: calling cyk_init, and then calling CYK_SET(i, i, k) for each i
+// if the ith token is symbol k
+void cyk_run(Rule *rs, int R)
+{
+	for (int sz = 2; sz <= CYK_N; ++sz)
+		for (int i = 0, j = i+sz-1; j < CYK_N; ++i, ++j)
+			for (int k = i; k < j; ++k)
+				if (CYK_AUX(i, k) && CYK_AUX(k+1, j))
+					for (int p = 0; p < R; ++p) {
+						Rule &r = rs[p];
+
+						if (CYK(i, k, r.A) && CYK(k+1, j, r.B))
+							CYK_SET(i, j, r.X);
+					}
+
+	// post-cond: CYK(0, N-1, k) == true iff the symbol k produces the input
+}
